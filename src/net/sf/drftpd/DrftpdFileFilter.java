1305772c8901beeac06ebbd122828e905b6087cd
mog <mog>
2002-07-07 05:17:22 +0000

    renamed to proper name

--- src/net/sf/drftpd/DrftpdFileFilter.java
+++ src/net/sf/drftpd/DrftpdFileFilter.java
@@ -3,7 +3,7 @@ package net.sf.drftpd;
 import java.io.FileFilter;
 import java.io.File;
 
-public class DftpdFileFilter implements FileFilter {
+public class DrftpdFileFilter implements FileFilter {
     public boolean accept(File file) {
 	if(file.getName().equals(".dftpd") || file.getName().equals(".placeholder")) {
 	    return false;
