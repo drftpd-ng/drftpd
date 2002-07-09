39dd1c299a325fe5e998ab78612757fa46f8081b
mog <mog>
2002-07-09 11:45:01 +0000

    misc added files

---
+++ rsync.sh
@@ -0,0 +1,1 @@
+rsync --delete -vre ssh --exclude=dftpd.conf 213.114.146.27:/home/mog/drftpd/ .
---
+++ src/net/sf/drftpd/PermissionDeniedException.java
@@ -0,0 +1,14 @@
+package net.sf.drftpd;
+
+/**
+ * @author mog
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class PermissionDeniedException extends Exception {
+	public PermissionDeniedException() { super(); }
+	public PermissionDeniedException(String message) { super(message); }
+}
