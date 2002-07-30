ce359e654f276ad4ae4b12493a6db62df2b8b0ab
mog <mog>
2002-07-30 13:29:07 +0000

    new files

---
+++ src/net/sf/drftpd/TransferStatus.java
@@ -0,0 +1,31 @@
+package net.sf.drftpd;
+
+import java.io.Serializable;
+
+/**
+ * @author mog
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class TransferStatus implements Serializable {
+	private long transfered;
+	private int elapsedTime;
+	private String filename;
+	
+	public TransferStatus(String filename, int elapsedTime, long transfered) {
+		this.filename = filename;
+		this.elapsedTime = elapsedTime;
+		this.transfered = transfered;
+	}
+	
+	public int getElapsedTime() {
+		return elapsedTime;
+	}
+	
+	public float getTransferSpeed() {
+		return transfered/elapsedTime;
+	}
+}
