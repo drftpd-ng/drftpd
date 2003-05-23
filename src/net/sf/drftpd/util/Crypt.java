4598b8eee37b8af81d4409872300a41e7d6d4dbf
mog <mog>
2003-05-23 13:34:18 +0000

    *** empty log message ***

--- src/net/sf/drftpd/util/Crypt.java
+++ src/net/sf/drftpd/util/Crypt.java
@@ -1,7 +1,6 @@
 package net.sf.drftpd.util;
 
 
-
 /****************************************************************************
  * jcrypt.java
  *
@@ -620,7 +619,7 @@ public class Crypt
 
    public static void main(String args[])
    {
-      if(args.length >= 2)
+      if(args.length == 2)
       {
          System.out.println
          (
