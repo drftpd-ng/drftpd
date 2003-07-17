6ff5654a1b118396d43aab971a234a7e1d1c5f95
mog <mog>
2003-07-17 01:27:00 +0000

    *** empty log message ***

---
+++ src/net/sf/drftpd/master/queues/NukeLog.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 2003-jul-13
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package net.sf.drftpd.master.queues;
+
+/**
+ * @author mog
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class NukeLog {
+
+}
