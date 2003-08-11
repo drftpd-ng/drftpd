b1207f3d6b2cd90f6a6a00ee953268750232c067
mog <mog>
2003-08-11 13:07:44 +0000

    *** empty log message ***

---
+++ src/net/sf/drftpd/slave/Root.java
@@ -0,0 +1,38 @@
+/*
+ * Created on 2003-aug-10
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package net.sf.drftpd.slave;
+
+import se.mog.io.File;
+
+/**
+ * @author mog
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class Root {
+	private String _root;
+	private long _minSpaceFree=0;
+	private int _priority=0;
+	
+	public Root(String root, long minSpaceFree, int priority) {
+		_root = root;
+	}
+	public File getFile() {
+		return new File(_root);
+	}
+	public String getPath() {
+		return _root;
+	}
+	
+	public long getMinSpaceFree() {
+		return _minSpaceFree;
+	}
+	public int getPriority() {
+		return _priority;
+	}
+}
