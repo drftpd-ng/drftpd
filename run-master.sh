b826079f68da1360b8200eecd0afc61668a3d2aa
mog <mog>
2003-08-31 21:19:51 +0000

    *** empty log message ***

--- run-master.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source env.sh
-export VMARGS="-Djava.library.path=. -Djava.rmi.server.hostname=213.114.146.8 -Djava.rmi.server.randomIDs=true -Djava.rmi.server.disableHttp=true -Djava.rmi.server.logCalls=false"
-exec java ${VMARGS} "$@" net.sf.drftpd.master.ConnectionManager
--- slave.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-VMARGS="
-	-server
-	-Djava.library.path=. \
-	-Djava.rmi.dgc.leaseValue=300000 \
-	-Djava.rmi.server.randomIDs=true"
-
-export CLASSPATH=classes
-exec java $VMARGS net.sf.drftpd.slave.SlaveImpl drftpd.conf
