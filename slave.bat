0a27d022a948381edea49f16931b86e3d5f52b56
mog <mog>
2003-09-02 12:02:27 +0000

    *** empty log message ***

--- slave.bat
+++ slave.bat
@@ -1,4 +1,4 @@
 set VMARGS=-Djava.library.path=. -Djava.rmi.dgc.leaseValue=300000 -Djava.rmi.server.randomIDs=true
-java net.sf.drftpd.slave.SlaveImpl drftpd.conf
+java %VMARGS% net.sf.drftpd.slave.SlaveImpl drftpd.conf
 pause
 
