fdd10848e34012cfafb7814e7a5f40ca3e798bab
mog <mog>
2003-10-25 08:23:45 +0000

    organize imports

--- src/net/sf/drftpd/master/command/plugins/Invite.java
+++ src/net/sf/drftpd/master/command/plugins/Invite.java
@@ -1,13 +1,5 @@
 package net.sf.drftpd.master.command.plugins;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
-import java.lang.ArrayIndexOutOfBoundsException;
-
-import net.sf.drftpd.Bytes;
-import net.sf.drftpd.ObjectExistsException;
-import net.sf.drftpd.ObjectNotFoundException;
 import net.sf.drftpd.event.Event;
 import net.sf.drftpd.master.BaseFtpConnection;
 import net.sf.drftpd.master.FtpReply;
@@ -15,17 +7,8 @@ import net.sf.drftpd.master.command.CommandHandler;
 import net.sf.drftpd.master.command.CommandManager;
 import net.sf.drftpd.master.command.CommandManagerFactory;
 import net.sf.drftpd.master.command.UnhandledCommandException;
-import net.sf.drftpd.master.usermanager.AbstractUser;
-import net.sf.drftpd.master.usermanager.NoSuchUserException;
-import net.sf.drftpd.master.usermanager.User;
-import net.sf.drftpd.master.usermanager.UserFileException;
-import net.sf.drftpd.remotefile.LinkedRemoteFile;
 
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
 
 /**
  * @author mog
@@ -36,8 +19,10 @@ import org.jdom.input.SAXBuilder;
 public class Invite implements CommandHandler {
 	public Invite() {
 	}
-	public void unload() {}
-	public void load(CommandManagerFactory initializer) {}
+	public void unload() {
+	}
+	public void load(CommandManagerFactory initializer) {
+	}
 
 	private Logger logger = Logger.getLogger(Invite.class);
 
@@ -51,8 +36,7 @@ public class Invite implements CommandHandler {
 
 	public FtpReply doSITE_INVITE(BaseFtpConnection conn) {
 		String user = conn.getRequest().getArgument();
-		Event invite =
-			new Event("INVITE " + user);
+		Event invite = new Event("INVITE " + user);
 		conn.getConnectionManager().dispatchFtpEvent(invite);
 		return new FtpReply(200, "Inviting " + user);
 	}
