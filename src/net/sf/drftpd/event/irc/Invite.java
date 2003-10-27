64e9081dc658b16b550fa7ec67502712fc480e49
mog <mog>
2003-10-27 02:38:48 +0000

    modularized /msg !invite, untested removed unused AutoJoin that was made a part of martyr

---
+++ src/net/sf/drftpd/event/irc/Invite.java
@@ -0,0 +1,99 @@
+/*
+ * Created on 2003-okt-27
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package net.sf.drftpd.event.irc;
+
+import java.io.IOException;
+
+import net.sf.drftpd.Initializeable;
+import net.sf.drftpd.event.InviteEvent;
+import net.sf.drftpd.master.ConnectionManager;
+import net.sf.drftpd.master.usermanager.NoSuchUserException;
+import net.sf.drftpd.master.usermanager.User;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import f00f.net.irc.martyr.GenericCommandAutoService;
+import f00f.net.irc.martyr.IRCConnection;
+import f00f.net.irc.martyr.InCommand;
+import f00f.net.irc.martyr.commands.MessageCommand;
+
+/**
+ * @author mog
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Invite extends GenericCommandAutoService implements Initializeable {
+
+	private Logger logger = Logger.getLogger(Invite.class);
+
+	private ConnectionManager _cm;
+
+	/**
+	 * @param connection
+	 */
+	public Invite(IRCConnection connection) {
+		super(connection);
+	}
+
+	/* (non-Javadoc)
+	 * @see f00f.net.irc.martyr.GenericCommandAutoService#updateCommand(f00f.net.irc.martyr.InCommand)
+	 */
+	protected void updateCommand(InCommand command) {
+		if(command instanceof MessageCommand) {
+			MessageCommand msgc = (MessageCommand)command;
+			String msg = msgc.getMessage();
+			if (msg.startsWith("!invite ")
+				&& msgc.isPrivateToUs(this.getConnection().getClientState())) {
+				String args[] = msg.split(" ");
+				User user;
+				try {
+					user = _cm.getUserManager().getUserByName(args[1]);
+				} catch (NoSuchUserException e) {
+					logger.log(
+						Level.WARN,
+						args[1] + " " + e.getMessage(),
+						e);
+					return;
+				} catch (IOException e) {
+					logger.log(Level.WARN, "", e);
+					return;
+				}
+				if (user.checkPassword(args[2])) {
+					logger.info(
+						"Invited "
+							+ msgc.getSourceString()
+							+ " as user "
+							+ user.getUsername());
+					//_conn.sendCommand(
+					//	new InviteCommand(msgc.getSource(), _channelName));
+					getConnectionManager().dispatchFtpEvent(
+						new InviteEvent(
+							"INVITE",
+							msgc.getSource().getNick()));
+				} else {
+					logger.log(
+						Level.WARN,
+						msgc.getSourceString()
+							+ " attempted invite with bad password: "
+							+ msgc);
+				}
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sf.drftpd.Initializeable#init(net.sf.drftpd.master.ConnectionManager)
+	 */
+	public void init(ConnectionManager connectionManager) {
+		_cm = connectionManager;
+	}
+	private ConnectionManager getConnectionManager() {
+		return _cm;
+	}
+}
