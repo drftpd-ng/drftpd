9c35e674d0fd6e21f4f55eff62afa89d525739c9
mog <mog>
2003-12-08 23:27:32 +0000

    Use -keyalg rsa

--- genkey.sh
+++ genkey.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-keytool -genkey -alias drftpd -dname CN=drftpd -keypass drftpd -keystore drftpd.key -storepass drftpd "$@"
+keytool -genkey -alias drftpd -dname CN=drftpd -keypass drftpd -keystore drftpd.key -storepass drftpd -keyalg rsa "$@"
