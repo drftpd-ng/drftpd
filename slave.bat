731872dba7f7847745c445a1579deaf802ceca9c
zubov <zubov>
2003-12-31 15:29:53 +0000

    thank you sasuke` was reading the wrong .conf file :)

--- slave.bat
+++ slave.bat
@@ -1,5 +1,4 @@
 set VMARGS=-Djava.library.path=lib -Djava.rmi.dgc.leaseValue=300000 -Djava.rmi.server.randomIDs=true
 set CLASSPATH=classes;lib/log4j-1.2.8.jar
-java %VMARGS% net.sf.drftpd.slave.SlaveImpl drftpd.conf
+java %VMARGS% net.sf.drftpd.slave.SlaveImpl
 pause
-
