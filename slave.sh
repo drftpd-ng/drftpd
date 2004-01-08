62c61c85316fe67f54ee14b3ca0100540f8237f1
mog <mog>
2004-01-08 03:20:56 +0000

    Moved log4j conf into conf/ Removed old startup scripts.

--- slave.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-VMARGS="-server -Djava.library.path=lib -Djava.rmi.dgc.leaseValue=300000 -Djava.rmi.server.randomIDs=true"
-export CLASSPATH=classes:lib/log4j-1.2.8.jar
-exec java ${VMARGS} net.sf.drftpd.slave.SlaveImpl
