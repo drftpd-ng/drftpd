90cdad817bcdd98b8dbc9ad513e16f66443dab37
zubov <zubov>
2004-01-29 22:47:40 +0000

    DatedDir update

--- src/net/sf/drftpd/event/listeners/PreTime.java
+++ src/net/sf/drftpd/event/listeners/PreTime.java
@@ -23,107 +23,9 @@ import f00f.net.irc.martyr.InCommand;
 import f00f.net.irc.martyr.commands.MessageCommand;
 
 /**
- * @version $Id: PreTime.java,v 1.12 2004/01/20 16:07:17 zubov Exp $
+ * @version $Id: PreTime.java,v 1.13 2004/01/29 22:47:40 zubov Exp $
  */
 public class PreTime implements FtpListener {
-	private SiteBot _siteBot;
-	private IRCListener _irc;
-	private static final Logger logger = Logger.getLogger(PreTime.class);
-	private ConnectionManager _cm;
-	private ArrayList datedDirs;
-	private String prebot;
-
-	public PreTime() throws FileNotFoundException, IOException {
-		super();
-	}
-
-	public void actionPerformed(Event event) {
-		if ("RELOAD".equals(event.getCommand())) {
-			reload();
-			return;
-		}
-		if (!(event instanceof DirectoryFtpEvent))
-			return;
-		DirectoryFtpEvent dfe = (DirectoryFtpEvent) event;
-		if (!getConnectionManager()
-			.getConfig()
-			.checkDirLog(dfe.getUser(), dfe.getDirectory())) {
-			return;
-		}
-		try {
-			if (dfe.getCommand().startsWith("MKD")) {
-				String release[] = dfe.getDirectory().getPath().split("/");
-				String releaseName;
-				if (isDatedDir(release[1])) {
-					releaseName = release[3];
-					if (release.length > 4)
-						return; // CD1 || CD2 type directories
-				} else {
-					releaseName = release[2];
-					if (release.length > 3)
-						return; // CD1 || CD2 type directories
-				}
-				if (releaseName == null)
-					return; // DatedDir section created date dir
-
-				_irc.getIRCConnection().sendCommand(
-					new MessageCommand(getPreBot(), "!pred " + releaseName));
-			}
-		} catch (ArrayIndexOutOfBoundsException ex) {
-			// do nothing just ignore, it's a directory created in /
-		}
-	}
-
-	public ConnectionManager getConnectionManager() {
-		return _cm;
-	}
-
-	public void init(ConnectionManager mgr) {
-		_cm = mgr;
-		reload();
-	}
-
-	private void reload() {
-		Properties props = new Properties();
-		try {
-			props.load(new FileInputStream("conf/pretime.conf"));
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		prebot = FtpConfig.getProperty(props, "prebot");
-		if (prebot == null) {
-			throw new FatalException("prebot not set in prebot.conf");
-		}
-		datedDirs = new ArrayList();
-		for (int i = 1;; i++) {
-			String temp = FtpConfig.getProperty(props, "DatedDir." + i);
-			if (temp == null)
-				break;
-			datedDirs.add(temp);
-		}
-
-		if (_siteBot != null) {
-			_siteBot.disable();
-		}
-		try {
-			_irc = (IRCListener) _cm.getFtpListener(IRCListener.class);
-			_siteBot = new SiteBot(_irc, this);
-		} catch (ObjectNotFoundException e1) {
-			logger.warn("Error loading sitebot component", e1);
-		}
-	}
-
-	public String getPreBot() {
-		return prebot;
-	}
-
-	public boolean isDatedDir(String section) {
-		for (Iterator iter = datedDirs.iterator(); iter.hasNext();) {
-			if (((String) iter.next()).equals(section))
-				return true;
-		}
-		return false;
-	}
 
 	public static class SiteBot extends GenericCommandAutoService {
 
@@ -174,6 +76,107 @@ public class PreTime implements FtpListener {
 					}
 				}
 			}
+		}
+	}
+	private static final Logger logger = Logger.getLogger(PreTime.class);
+	private ConnectionManager _cm;
+	private IRCListener _irc;
+	private SiteBot _siteBot;
+	private ArrayList datedDirs;
+	private String prebot;
+
+	public PreTime() throws FileNotFoundException, IOException {
+		super();
+	}
+
+	public void actionPerformed(Event event) {
+		if ("RELOAD".equals(event.getCommand())) {
+			reload();
+			return;
+		}
+		if (!(event instanceof DirectoryFtpEvent))
+			return;
+		DirectoryFtpEvent dfe = (DirectoryFtpEvent) event;
+		if (!getConnectionManager()
+			.getConfig()
+			.checkDirLog(dfe.getUser(), dfe.getDirectory())) {
+			return;
+		}
+		try {
+			if (dfe.getCommand().startsWith("MKD")) {
+				String release[] = dfe.getDirectory().getPath().split("/");
+				String releaseName;
+				if (isDatedDir(release[1])) {
+					releaseName = release[3];
+					if (release.length > 4)
+						return; // CD1 || CD2 type directories
+				} else {
+					releaseName = release[2];
+					if (release.length > 3)
+						return; // CD1 || CD2 type directories
+				}
+				if (releaseName == null)
+					return; // DatedDir section created date dir
+
+				_irc.getIRCConnection().sendCommand(
+					new MessageCommand(getPreBot(), "!pred " + releaseName));
+			}
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			// do nothing just ignore, it's a directory created in /
+		}
+	}
+
+	public ConnectionManager getConnectionManager() {
+		return _cm;
+	}
+
+	public String getPreBot() {
+		return prebot;
+	}
+
+	public void init(ConnectionManager mgr) {
+		_cm = mgr;
+		reload();
+	}
+
+	public boolean isDatedDir(String section) {
+		for (Iterator iter = datedDirs.iterator(); iter.hasNext();) {
+			if (((String) iter.next()).equals(section))
+				return true;
+		}
+		return false;
+	}
+
+	private void reload() {
+		Properties props = new Properties();
+		try {
+			props.load(new FileInputStream("conf/pretime.conf"));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		prebot = FtpConfig.getProperty(props, "prebot");
+		if (prebot == null) {
+			throw new FatalException("prebot not set in prebot.conf");
+		}
+		datedDirs = new ArrayList();
+		for (int i = 1;; i++) {
+			String temp = null;
+			try {
+				temp = FtpConfig.getProperty(props, "DatedDir." + i);
+			} catch (NullPointerException e2) {
+				break;
+			}
+			datedDirs.add(temp);
+		}
+
+		if (_siteBot != null) {
+			_siteBot.disable();
+		}
+		try {
+			_irc = (IRCListener) _cm.getFtpListener(IRCListener.class);
+			_siteBot = new SiteBot(_irc, this);
+		} catch (ObjectNotFoundException e1) {
+			logger.warn("Error loading sitebot component", e1);
 		}
 	}
 }
