906fbb2fa7beb4fd4ba85161bd77bfc52a6593bc
zubov <zubov>
2004-03-21 08:06:38 +0000

    updated CycleFilter to not only Cycle on slaves with 0 bandwidth usage, but on slaves that tie scores for whatever reasons

--- src/org/drftpd/slaveselection/filter/CycleFilter.java
+++ src/org/drftpd/slaveselection/filter/CycleFilter.java
@@ -18,19 +18,19 @@
 package org.drftpd.slaveselection.filter;
 
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Properties;
 
 import net.sf.drftpd.NoAvailableSlaveException;
-import net.sf.drftpd.SlaveUnavailableException;
 import net.sf.drftpd.master.usermanager.User;
 import net.sf.drftpd.remotefile.LinkedRemoteFileInterface;
 
 /**
  * Checks ScoreChart for slaves with 0 bw usage and assigns 1 extra point to the one in that has been unused for the longest time.
  * 
- * @author mog
- * @version $Id: CycleFilter.java,v 1.3 2004/03/04 01:41:27 zubov Exp $
+ * @author mog, zubov
+ * @version $Id: CycleFilter.java,v 1.4 2004/03/21 08:06:38 zubov Exp $
  */
 public class CycleFilter extends Filter {
 
@@ -45,34 +45,36 @@ public class CycleFilter extends Filter {
 		LinkedRemoteFileInterface dir)
 		throws NoAvailableSlaveException {
 
-		ScoreChart.SlaveScore leastUsed = null;
-		for (Iterator iter = scorechart.getSlaveScores().iterator();
-			iter.hasNext();
-			) {
-			ScoreChart.SlaveScore score = (ScoreChart.SlaveScore) iter.next();
-			try {
-				if (score
-					.getRSlave()
-					.getStatus()
-					.getThroughputDirection(direction)
-					== 0) {
-
-					if (leastUsed == null) {
-						leastUsed = score;
-					} else if (
-						score.getRSlave().getLastTransferForDirection(
-							direction)
-							< leastUsed.getRSlave().getLastTransferForDirection(
-								direction)) {
-						leastUsed = score;
-					}
+		ArrayList tempList = new ArrayList(scorechart.getSlaveScores());
+		while (true) {
+			if (tempList.isEmpty())
+				return;
+			ScoreChart.SlaveScore first =
+				(ScoreChart.SlaveScore) tempList.get(0);
+			ArrayList equalList = new ArrayList();
+			equalList.add(first);
+			tempList.remove(first);
+			for (Iterator iter = tempList.iterator(); iter.hasNext();) {
+				ScoreChart.SlaveScore match =
+					(ScoreChart.SlaveScore) iter.next();
+				if (match.compareTo(first) == 0) {
+					equalList.add(match);
+					iter.remove();
 				}
-			} catch (SlaveUnavailableException e) {
-				iter.remove();
 			}
-		}
-		if (leastUsed != null) {
-			leastUsed.addScore(1);
+			ScoreChart.SlaveScore leastUsed = first;
+			for (Iterator iter = equalList.iterator(); iter.hasNext();) {
+				ScoreChart.SlaveScore match =
+					(ScoreChart.SlaveScore) iter.next();
+				if (match.getRSlave().getLastTransferForDirection(direction)
+					< leastUsed.getRSlave().getLastTransferForDirection(
+						direction)) {
+					leastUsed = match;
+				}
+			}
+			if (leastUsed != null) {
+				leastUsed.addScore(1);
+			}
 		}
 	}
 }
