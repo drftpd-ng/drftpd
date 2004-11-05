bb638af5e349b1047dd982569fb27d884b76e934
mog <mog>
2004-11-05 14:07:23 +0000

    the new usermanagers

---
+++ src/org/drftpd/usermanager/jsx2/JSXUser.java
@@ -0,0 +1,150 @@
+/*
+ * This file is part of DrFTPD, Distributed FTP Daemon.
+ *
+ * DrFTPD is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * DrFTPD is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * DrFTPD; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.drftpd.usermanager.jsx2;
+
+import JSX.ObjOut;
+
+import net.sf.drftpd.util.Crypt;
+import net.sf.drftpd.util.SafeFileWriter;
+
+import org.apache.log4j.Logger;
+
+import org.drftpd.usermanager.AbstractUser;
+import org.drftpd.usermanager.PlainTextPasswordUser;
+import org.drftpd.usermanager.UnixPassword;
+import org.drftpd.usermanager.UserFileException;
+import org.drftpd.usermanager.UserManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import java.util.ArrayList;
+
+
+/**
+ * @author mog
+ * @version $Id: JSXUser.java,v 1.1 2004/11/05 14:07:22 mog Exp $
+ */
+public class JSXUser extends AbstractUser implements PlainTextPasswordUser,
+    UnixPassword {
+    private String _password;
+    private String _unixPassword;
+    private transient UserManager _usermanager;
+    private boolean _purged;
+
+    public JSXUser(JSXUserManager usermanager, String username) {
+        super(username);
+        _usermanager = usermanager;
+    }
+
+    public boolean checkPassword(String password) {
+        if (this._password == null) {
+            if (this._unixPassword == null) {
+                throw new IllegalStateException("no password set");
+            }
+
+            if (this._unixPassword.equals(Crypt.crypt(
+                            this._unixPassword.substring(0, 2), password))) {
+                setPassword(password);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        return this._password.equals(password);
+    }
+
+    public void commit() throws UserFileException {
+        if (_purged) {
+            return;
+        }
+
+        try {
+            if (!(_usermanager instanceof JSXUserManager)) {
+                throw new ClassCastException(
+                    "JSXUser without reference to JSXUserManager");
+            }
+
+            ObjOut out = new ObjOut(new SafeFileWriter(((JSXUserManager) _usermanager).getUserFile(
+                            this.getUsername())));
+
+            try {
+                out.writeObject(this);
+            } finally {
+                out.close();
+            }
+
+            Logger.getLogger(JSXUser.class).debug("wrote " + getUsername());
+        } catch (IOException ex) {
+            throw new UserFileException("Error writing userfile for " +
+                this.getUsername() + ": " + ex.getMessage(), ex);
+        }
+    }
+
+    protected void finalize() throws Throwable {
+        commit();
+    }
+
+    public String getPassword() {
+        return this._password;
+    }
+
+    public String getUnixPassword() {
+        return _unixPassword;
+    }
+
+    public void purge() {
+        _purged = true;
+        _usermanager.remove(this);
+
+        if (!(_usermanager instanceof JSXUserManager)) {
+            throw new ClassCastException(
+                "JSXUser without reference to JSXUserManager");
+        }
+
+        File userfile = ((JSXUserManager) _usermanager).getUserFile(this.getUsername());
+        userfile.delete();
+    }
+
+    public void setPassword(String password) {
+        this._unixPassword = null;
+        this._password = password;
+    }
+
+    public void setUnixPassword(String password) {
+        this._password = null;
+        this._unixPassword = password;
+    }
+
+    void setUserManager(UserManager um) {
+        _usermanager = um;
+    }
+
+    public void update() {
+        //an update was made, but commit() should be called from all places so
+        // we don't need to do anything.
+        //if we do, make sure it's implemented in all set and update methods in
+        // AbstractUser
+    }
+
+    public UserManager getUserManager() {
+        return _usermanager;
+    }
+}
---
+++ src/org/drftpd/usermanager/jsx2/JSXUserManager.java
@@ -0,0 +1,182 @@
+/*
+ * This file is part of DrFTPD, Distributed FTP Daemon.
+ *
+ * DrFTPD is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * DrFTPD is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * DrFTPD; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.drftpd.usermanager.jsx2;
+
+import JSX.ObjIn;
+
+import net.sf.drftpd.DuplicateElementException;
+import net.sf.drftpd.FatalException;
+import net.sf.drftpd.master.ConnectionManager;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import org.drftpd.usermanager.NoSuchUserException;
+import org.drftpd.usermanager.User;
+import org.drftpd.usermanager.UserFileException;
+import org.drftpd.usermanager.UserManager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+
+/**
+ * @author mog
+ * @version $Id: JSXUserManager.java,v 1.1 2004/11/05 14:07:22 mog Exp $
+ */
+public class JSXUserManager extends UserManager {
+    private static final Logger logger = Logger.getLogger(JSXUserManager.class.getName());
+    private ConnectionManager _connManager;
+    private String _userpath = "users/jsx2/";
+    private File _userpathFile = new File(_userpath);
+
+    public JSXUserManager() throws UserFileException {
+        if (!_userpathFile.exists() && !_userpathFile.mkdirs()) {
+            throw new UserFileException(new IOException(
+                    "Error creating folders: " + _userpathFile));
+        }
+
+        String[] userfilenames = _userpathFile.list();
+        int numUsers = 0;
+
+        for (int i = 0; i < userfilenames.length; i++) {
+            String string = userfilenames[i];
+
+            if (string.endsWith(".xml")) {
+                numUsers++;
+            }
+        }
+
+        if (numUsers == 0) {
+            User user = createUser("drftpd");
+            user.setGroup("drftpd");
+            user.setPassword("drftpd");
+            user.setRatio(0);
+
+            try {
+                user.addIPMask("*@127.0.0.1");
+                user.addIPMask("*@0:0:0:0:0:0:0:1");
+            } catch (DuplicateElementException e) {
+            }
+
+            try {
+                user.addSecondaryGroup("siteop");
+            } catch (DuplicateElementException e1) {
+            }
+
+            user.commit();
+        }
+    }
+
+    public User createUser(String username) {
+        JSXUser user = new JSXUser(this, username);
+
+        return user;
+    }
+
+    public void delete(String username) {
+        getUserFile(username).delete();
+    }
+
+    public Collection getAllUsers() throws UserFileException {
+        ArrayList users = new ArrayList();
+        String[] userpaths = _userpathFile.list();
+
+        for (int i = 0; i < userpaths.length; i++) {
+            String userpath = userpaths[i];
+
+            if (!userpath.endsWith(".xml")) {
+                continue;
+            }
+
+            String username = userpath.substring(0,
+                    userpath.length() - ".xml".length());
+
+            try {
+                users.add((JSXUser) getUserByNameUnchecked(username));
+
+                // throws IOException
+            } catch (NoSuchUserException e) {
+            } // continue
+        }
+
+        return users;
+    }
+
+    public User getUserByNameUnchecked(String username)
+        throws NoSuchUserException, UserFileException {
+        try {
+            JSXUser user = (JSXUser) _users.get(username);
+
+            if (user != null) {
+                return user;
+            }
+
+            ObjIn in;
+
+            try {
+                in = new ObjIn(new FileReader(getUserFile(username)));
+            } catch (FileNotFoundException ex) {
+                throw new NoSuchUserException("No such user");
+            }
+
+            try {
+                user = (JSXUser) in.readObject();
+
+                //throws RuntimeException
+                user.setUserManager(this);
+                _users.put(user.getUsername(), user);
+                user.reset(_connManager);
+
+                return user;
+            } catch (ClassNotFoundException e) {
+                throw new FatalException(e);
+            }
+        } catch (Throwable ex) {
+            if (ex instanceof NoSuchUserException) {
+                throw (NoSuchUserException) ex;
+            }
+
+            throw new UserFileException("Error loading " + username, ex);
+        }
+    }
+
+    protected File getUserFile(String username) {
+        return new File(_userpath + username + ".xml");
+    }
+
+    public void saveAll() throws UserFileException {
+        logger.log(Level.INFO, "Saving userfiles");
+
+        for (Iterator iter = _users.values().iterator(); iter.hasNext();) {
+            Object obj = iter.next();
+
+            if (!(obj instanceof JSXUser)) {
+                throw new ClassCastException("Only accepts JSXUser objects");
+            }
+
+            JSXUser user = (JSXUser) obj;
+            user.commit();
+        }
+    }
+}
