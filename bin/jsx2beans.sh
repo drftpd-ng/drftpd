35f3ccaf97252d167938fd50281ae49d4961c194
zubov <zubov>
2005-02-21 18:59:29 +0000

    jsx2beans

---
+++ bin/jsx2beans.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ant compile-jsx
+export 
+CLASSPATH="classes:lib/log4j-1.2.8.jar:lib/oro.jar:lib/jdom.jar:lib/JSX1.0.7.4.jar:lib/replacer.jar"
+exec java $@ org.drftpd.usermanager.UserManagerConverter net.sf.drftpd.master.usermanager.jsx.JSXUserManager org.drftpd.usermanager.javabeans.BeanUserManager
