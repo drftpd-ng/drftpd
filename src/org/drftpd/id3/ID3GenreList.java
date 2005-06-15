7cd0f77cee505590aab0b7f4eb0a675067360341
zubov <zubov>
2005-06-15 21:38:30 +0000

    updated ID3 Genre list, thanks Dark

--- src/org/drftpd/id3/ID3GenreList.java
+++ src/org/drftpd/id3/ID3GenreList.java
@@ -27,41 +27,50 @@ import org.apache.log4j.Logger;
 public class ID3GenreList {
     public static final String[] genres = {
             "Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk",
-            "Grunge", "Hip-Hop", "Jazz", "Metal", "New Age", "Oldies", "Other",
-            "Pop", "R&B", "Rap", "Reggae", "Rock", "Techno", "Industrial",
-            "Alternative", "Ska", "Death Metal", "Pranks", "Soundtrack",
-            "Euro-Techno", "Ambient", "Trip-Hop", "Vocal", "Jazz+Funk", "Fusion",
-            "Trance", "Classical", "Instrumental", "Acid", "House", "Game",
-            "Sound Clip", "Gospel", "Noise", "AlternRock", "Bass", "Soul",
-            "Punk", "Space", "Meditative", "Instrumental Pop",
-            "Instrumental Rock", "Ethnic", "Gothic", "Darkwave",
-            "Techno-Industrial", "Electronic", "Pop-Folk", "Eurodance", "Dream",
+            "Grunge", "Hip-Hop", "Jazz", "Metal", "New Age", "Oldies",
+            "Other", "Pop", "R&B", "Rap", "Reggae", "Rock", "Techno",
+            "Industrial", "Alternative", "Ska", "Death Metal", "Pranks",
+            "Soundtrack", "Euro-Techno", "Ambient", "Trip-Hop", "Vocal",
+            "Jazz+Funk", "Fusion", "Trance", "Classical", "Instrumental",
+            "Acid", "House", "Game", "Sound Clip", "Gospel", "Noise",
+            "Alternative Rock", "Bass", "Soul", "Punk", "Space",
+            "Meditative", "Instrumental Pop", "Instrumental Rock",
+            "Ethnic", "Gothic", "Darkwave", "Techno-Industrial",
+            "Electronic", "Pop-Folk", "Eurodance", "Dream",
             "Southern Rock", "Comedy", "Cult", "Gangsta", "Top 40",
-            "Christian Rap", "Pop/Funk", "Jungle", "Native American", "Cabaret",
-            "New Wave", "Psychedelic", "Rave", "Showtunes", "Trailer", "Lo-Fi",
-            "Tribal", "Acid Punk", "Acid Jazz", "Polka", "Retro", "Musical",
-            "Rock & Roll", "Hard Rock", "Folk", "Folk-Rock", "National Folk",
-            "Swing", "Fast Fusion", "Bebob", "Latin", "Revival", "Celtic",
-            "Bluegrass", "Avantgarde", "Gothic Rock", "Progressive Rock",
-            "Psychedelic Rock", "Symphonic Rock", "Slow Rock", "Big Band",
-            "Chorus", "Easy Listening", "Acoustic", "Humour", "Speech",
-            "Chanson", "Opera", "Chamber Music", "Sonata", "Symphony",
-            "Booty Brass", "Primus", "Porn Groove", "Satire", "Slow Jam", "Club",
-            "Tango", "Samba", "Folklore", "Ballad", "Power Ballad",
-            "Rhytmic Soul", "Freestyle", "Duet", "Punk Rock", "Drum Solo",
-            "Acapella", "Euro-House", "Dance Hall", "unknown"
+            "Christian Rap", "Pop/Funk", "Jungle", "Native US", "Cabaret",
+            "New Wave", "Psychadelic", "Rave", "Showtunes", "Trailer",
+            "Lo-Fi", "Tribal", "Acid Punk", "Acid Jazz", "Polka", "Retro",
+            "Musical", "Rock & Roll", "Hard Rock", "Folk", "Folk-Rock",
+            "National Folk", "Swing", "Fast Fusion", "Bebob", "Latin",
+            "Revival", "Celtic", "Bluegrass", "Avantgarde", "Gothic Rock",
+            "Progressive Rock", "Psychedelic Rock", "Symphonic Rock",
+            "Slow Rock", "Big Band", "Chorus", "Easy Listening",
+            "Acoustic", "Humour", "Speech", "Chanson", "Opera",
+            "Chamber Music", "Sonata", "Symphony", "Booty Bass", "Primus",
+            "Porn Groove", "Satire", "Slow Jam", "Club", "Tango", "Samba",
+            "Folklore", "Ballad", "Power Ballad", "Rhytmic Soul",
+            "Freestyle", "Duet", "Punk Rock", "Drum Solo", "Acapella",
+            "Euro-House", "Dance Hall", "Goa", "Drum & Bass",
+            "Club-House", "Hardcore", "Terror", "Indie", "BritPop",
+            "Negerpunk", "Polsk Punk", "Beat", "Christian Gangsta",
+            "Heavy Metal", "Black Metal", "Crossover", "Contemporary C",
+            "Christian Rock", "Merengue", "Salsa", "Thrash Metal",
+            "Anime", "JPop", "SynthPop"
         };
-	private static final Logger logger = Logger.getLogger(ID3GenreList.class);
+
+    private static final Logger logger = Logger.getLogger(ID3GenreList.class);
 
     /** Returns the Genre for given number
      *        @return Genre as String
      */
     public static String getGenre(int number) {
+        int index = number & 0xff;
         try {
-        	return genres[number];
+			return genres[index];
         } catch(ArrayIndexOutOfBoundsException e) {
-        	logger.warn("Unknown genre number: "+number);
-        	return "";
+			logger.warn("Unknown genre number: "+index);
+			return "";
         }
     }
 
