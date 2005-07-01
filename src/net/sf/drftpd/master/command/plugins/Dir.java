f0816dc2166069a4c42523d8620faca60eb19576
zubov <zubov>
2005-07-01 21:52:23 +0000

    how the hell did that get in there? and why didn't anyone notice?

--- src/net/sf/drftpd/master/command/plugins/Dir.java
+++ src/net/sf/drftpd/master/command/plugins/Dir.java
@@ -379,7 +379,7 @@ public class Dir implements CommandHandler, CommandHandlerFactory, Cloneable {
             return Reply.RESPONSE_530_ACCESS_DENIED;
         }
         
-        if (requestedFile.isDirectory() && requestedFile.getMap().size() == 0) {
+        if (requestedFile.isDirectory() && requestedFile.getMap().size() != 0) {
 			return new Reply(550, requestedFile.getPath()
 					+ ": Directory not empty");
 		}
