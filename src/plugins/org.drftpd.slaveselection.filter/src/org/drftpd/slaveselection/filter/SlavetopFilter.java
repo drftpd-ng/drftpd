1b9c9b72e6c19e500a1292aecf440e68b60281e1
zubov <zubov>
2007-12-02 13:47:47 +0000

    Committing SlavetopFilter

---
+++ src/plugins/org.drftpd.slaveselection.filter/src/org/drftpd/slaveselection/filter/SlavetopFilter.java
@@ -0,0 +1,145 @@
+/*
+ * This file is part of DrFTPD, Distributed FTP Daemon.
+ *
+ * DrFTPD is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * DrFTPD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DrFTPD; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.drftpd.slaveselection.filter;
+
+import java.io.FileNotFoundException;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.drftpd.GlobalContext;
+import org.drftpd.PropertyHelper;
+import org.drftpd.exceptions.NoAvailableSlaveException;
+import org.drftpd.exceptions.ObjectNotFoundException;
+import org.drftpd.master.RemoteSlave;
+import org.drftpd.sections.SectionInterface;
+import org.drftpd.slaveselection.filter.ScoreChart.SlaveScore;
+import org.drftpd.usermanager.User;
+import org.drftpd.vfs.DirectoryHandle;
+import org.drftpd.vfs.FileHandle;
+import org.drftpd.vfs.InodeHandle;
+import org.drftpd.vfs.InodeHandleInterface;
+import org.drftpd.vfs.VirtualFileSystem;
+import org.drftpd.vfs.VirtualFileSystemInode;
+
+
+/**
+ * @author mog
+ * @author zubov
+ * @version $Id$
+ */
+public class SlavetopFilter extends Filter {
+    private long _assign;
+    private int _topslaves;
+	private static final Logger logger = Logger.getLogger(SlavetopFilter.class
+			.getName());
+
+    public SlavetopFilter(int i, Properties p) {
+    	super(i,p);
+    	// topslaves is the # of slaves to assign points to
+        _topslaves = Integer.parseInt(PropertyHelper.getProperty(p, i +
+                    ".topslaves"));
+        _assign = Long.parseLong(PropertyHelper.getProperty(p, i + ".assign"));
+    }
+
+    public void process(ScoreChart scorechart, DirectoryHandle dir) {
+        //// find the section part of the path name
+        SectionInterface section = GlobalContext.getGlobalContext().getSectionManager().lookup(dir);
+        while (true) {
+        	if (dir.getParent().equals(section.getBaseDirectory())) {
+        		break;
+        	}
+        	if (dir.equals(GlobalContext.getGlobalContext().getRoot())) {
+        		// this is not a "release", don't process
+        		return;
+        	}
+        	dir = dir.getParent();
+        }
+
+        Hashtable<RemoteSlave, ScoreChart.SlaveScore> slavesmap =
+        	new Hashtable<RemoteSlave, ScoreChart.SlaveScore>();
+        
+        for (ScoreChart.SlaveScore slaveScore : scorechart.getSlaveScores()) {
+        	slavesmap.put(slaveScore.getRSlave(), new ScoreChart.SlaveScore(
+					slaveScore.getRSlave()));
+        }
+        
+        ArrayList<FileHandle> files = getAllFiles(dir);
+
+        for (FileHandle file : files) {
+        	try {
+				for (RemoteSlave rslave : file.getSlaves()) {
+					ScoreChart.SlaveScore score = (SlaveScore) slavesmap.get(rslave);
+				    if (score == null) {
+				    	// the slave was already removed from SlaveSelection
+				        continue;
+				    }
+				    score.addScore(1);
+				}
+			} catch (FileNotFoundException e) {
+				// file was removed, not much we can do, just continue
+			}
+        }
+        ArrayList<ScoreChart.SlaveScore> slavescores = 
+        	new ArrayList<ScoreChart.SlaveScore>(slavesmap.values());
+        ArrayList ss = slavescores;
+        Collections.sort(ss, Collections.reverseOrder());
+
+
+        if(_assign == 0) {
+        	for(ScoreChart.SlaveScore score : slavescores.subList(_topslaves, slavescores.size())) {
+        		scorechart.removeSlaveFromChart(score.getRSlave());
+        	}
+        }
+        Iterator iter = slavescores.iterator();
+        for (int i = 0; i < _topslaves && iter.hasNext(); i++) {
+            ScoreChart.SlaveScore score = (SlaveScore) iter.next();
+
+            try {
+                scorechart.getScoreForSlave(score.getRSlave()).addScore(_assign);
+            } catch (ObjectNotFoundException e1) {
+                logger.error("Unable to assign points to slave " + score.getRSlave().getName());
+            }
+        }
+    }
+
+	private ArrayList<FileHandle> getAllFiles(DirectoryHandle dir) {
+		ArrayList<FileHandle> files = new ArrayList<FileHandle>();
+		try {
+			for (InodeHandle inode : dir.getInodeHandlesUnchecked()) {
+				if (inode.isFile()) {
+					files.add((FileHandle) inode);
+				} else if (inode.isDirectory()) {
+					files.addAll(getAllFiles((DirectoryHandle) inode));
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// oh well, we just won't have any files to add
+		}
+		return files;
+	}
+
+	@Override
+	public void process(ScoreChart scorechart, User user, InetAddress peer, char direction, InodeHandleInterface inode, RemoteSlave sourceSlave) throws NoAvailableSlaveException {
+			process(scorechart, ((InodeHandle) inode).getParent());
+	}
+}
