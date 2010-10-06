1a274df20117c2203ad1c14a592a14e64bdebc05
cyber1331 <cyber1331>
2010-10-06 22:39:12 +0000

    Added DupeCheck plugin.  This checks to see if file/folder already exists on site. Options include excluded files/folders and scanning file/folders/both/none. Uses permissions in perms.conf for folder/user settings.

---
+++ src/plugins/org.drftpd.plugins.dupecheck/src/org/drftpd/plugins/dupecheck/DupeCheckCommands.java
@@ -0,0 +1,88 @@
+/*
+ * This file is part of DrFTPD, Distributed FTP Daemon.
+ *
+ * DrFTPD is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * DrFTPD is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * DrFTPD; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.drftpd.plugins.dupecheck;
+
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.drftpd.GlobalContext;
+import org.drftpd.commandmanager.CommandInterface;
+import org.drftpd.commandmanager.CommandRequest;
+import org.drftpd.commandmanager.CommandResponse;
+import org.drftpd.commandmanager.ImproperUsageException;
+import org.drftpd.plugins.dupecheck.metadata.DupeCheckFileData;
+import org.drftpd.vfs.DirectoryHandle;
+import org.drftpd.vfs.FileHandle;
+import org.drftpd.vfs.InodeHandle;
+import org.drftpd.vfs.index.AdvancedSearchParams;
+import org.drftpd.vfs.index.IndexEngineInterface;
+import org.drftpd.vfs.index.IndexException;
+
+public class DupeCheckCommands extends CommandInterface {
+	
+	/*
+	 * Site UNDUPE command to un-dupe specific files/folders
+	 * 
+	 * This uses lucene's search to figure out if the file exist, 
+	 * and then adds metadata to it so site knows its unduped.
+	 */
+    public CommandResponse doSITE_UNDUPE(CommandRequest request) throws ImproperUsageException {
+        if (!request.hasArgument()) {
+        	throw new ImproperUsageException();
+        }
+
+        StringTokenizer st = new StringTokenizer(request.getArgument());
+
+        if (!st.hasMoreTokens()) {
+            throw new ImproperUsageException();
+        }
+        
+        String arg = st.nextToken();
+        if (arg.length() < 3) {
+        	throw new ImproperUsageException();
+        }
+        
+        CommandResponse response = new CommandResponse(200, "Un-Dupe Complete");
+        
+		AdvancedSearchParams params = new AdvancedSearchParams();
+		params.setName(arg);
+		params.setExact(true);
+
+		try {
+			IndexEngineInterface ie = GlobalContext.getGlobalContext().getIndexEngine();
+			Map<String,String> inodes = ie.advancedFind(GlobalContext.getGlobalContext().getRoot(), params);
+			
+			if (!inodes.isEmpty()) {
+				for (Map.Entry<String,String> item : inodes.entrySet()) {        
+					InodeHandle inode = item.getValue().equals("d") ? new DirectoryHandle(item.getKey()) : new FileHandle(item.getKey());
+					try {
+						inode.addPluginMetaData(DupeCheckFileData.DUPE, false);
+						response.addComment("Unduped: " + inode.getPath());
+					} catch (FileNotFoundException e) {
+						// File Not Found - Deleted?? Probably not a good thing
+					}
+				}
+			}
+		} catch (IndexException e) {
+			//Index Exception while searching
+			response.addComment(e);
+		}
+        
+        return response;
+    }
+}
---
+++ src/plugins/org.drftpd.plugins.dupecheck/src/org/drftpd/plugins/dupecheck/DupeCheckHooks.java
@@ -0,0 +1,174 @@
+/*
+ * This file is part of DrFTPD, Distributed FTP Daemon.
+ *
+ * DrFTPD is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * DrFTPD is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * DrFTPD; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.drftpd.plugins.dupecheck;
+
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+import org.bushe.swing.event.annotation.EventSubscriber;
+import org.drftpd.GlobalContext;
+import org.drftpd.commandmanager.CommandRequest;
+import org.drftpd.commandmanager.CommandRequestInterface;
+import org.drftpd.commandmanager.CommandResponse;
+import org.drftpd.commandmanager.PreHookInterface;
+import org.drftpd.commandmanager.StandardCommandManager;
+import org.drftpd.dynamicdata.KeyNotFoundException;
+import org.drftpd.event.ReloadEvent;
+import org.drftpd.master.config.ConfigInterface;
+import org.drftpd.plugins.dupecheck.metadata.DupeCheckFileData;
+import org.drftpd.usermanager.User;
+import org.drftpd.vfs.DirectoryHandle;
+import org.drftpd.vfs.FileHandle;
+import org.drftpd.vfs.InodeHandle;
+import org.drftpd.vfs.index.AdvancedSearchParams;
+import org.drftpd.vfs.index.IndexEngineInterface;
+import org.drftpd.vfs.index.IndexException;
+
+/**
+ * @author CyBeR
+ * @version $Id: DupeCheck.java 1925 2009-06-15 21:46:05Z CyBeR $
+ */
+
+public class DupeCheckHooks implements PreHookInterface {
+	private static final Logger logger = Logger.getLogger(DupeCheckHooks.class);
+
+	private Pattern _exempt = null;
+	private int _type = 0;
+	
+	public void initialize(StandardCommandManager cManager) {
+		loadConf();
+	}
+	
+    @EventSubscriber
+	public void onReloadEvent(ReloadEvent event) {
+    	loadConf();
+    }
+	
+    /*
+     * Loads config from file and sets regex patterns
+     */
+	private void loadConf() {
+    	Properties cfg =  GlobalContext.getGlobalContext().getPluginsConfig().getPropertiesForPlugin("dupecheck.conf");
+        
+    	String exempt = cfg.getProperty("exempt");
+        String type = cfg.getProperty("type");
+        
+        if (exempt != null) {
+        	_exempt = null;
+        	_exempt = Pattern.compile(exempt.trim());
+        }
+
+        if (type != null) {
+        	_type = Integer.parseInt(type.trim());
+        }        
+    	
+	}
+
+	/*
+	 * Prehook method for Making a New DIR
+	 */
+	public CommandRequestInterface doDupeCheckMKD(CommandRequest request) {
+		if (((_type == 2) || (_type == 3)) && (!checkFile(request.getArgument()))) {
+			return doDupeCheck(request);
+		}
+		return request;
+	}
+
+	/*
+	 * Prehook method for Creating a New FILE
+	 */
+	public CommandRequestInterface doDupeCheckSTOR(CommandRequest request) {
+		if (((_type == 1) || (_type == 3)) && (!checkFile(request.getArgument()))) {
+			return doDupeCheck(request);
+		}
+		return request;
+	}	
+	
+	/*
+	 * Checks the file/dir to see if its in the regex exclude list
+	 */
+	private boolean checkFile(String file) {
+		if (_exempt != null) {
+			Matcher matcher = _exempt.matcher(file.toLowerCase());
+			if (matcher.find()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/*
+	 * Checks to see if file/dir is already in database
+	 * Then checks to see if user has permission to upload file/dir 
+	 */
+	private CommandRequestInterface doDupeCheck(CommandRequest request) {
+
+		try {
+			User user = request.getSession().getUserNull(request.getUser());
+
+			AdvancedSearchParams params = new AdvancedSearchParams();
+			params.setName(request.getArgument());
+			params.setExact(true);
+			
+			IndexEngineInterface ie = GlobalContext.getGlobalContext().getIndexEngine();
+			Map<String,String> inodes = ie.advancedFind(GlobalContext.getGlobalContext().getRoot(), params);
+			
+			if (!inodes.isEmpty()) {
+				for (Map.Entry<String,String> item : inodes.entrySet()) {
+					boolean isDupe = false;
+
+					InodeHandle inode = item.getValue().equals("d") ? new DirectoryHandle(item.getKey()) : new FileHandle(item.getKey());					
+					
+					try {
+						if (inode.getPluginMetaData(DupeCheckFileData.DUPE)) {
+							isDupe = true;
+						}
+					} catch (KeyNotFoundException e) {
+						// This is fine, means file/dir has not been un-duped
+						isDupe = true;
+					} catch (FileNotFoundException e) {
+						// File not found, not good, index refers to file that does not exist
+						logger.warn("Index Contained an unexistent inode: " + item.getKey());
+					}
+					
+					if (isDupe) {
+				        ConfigInterface config = GlobalContext.getConfig();
+						if (config.checkPathPermission("dupecheck", user, request.getCurrentDirectory())) {
+							request.setDeniedResponse(new CommandResponse(400,"DUPE: " + inode.getPath()));
+							request.setAllowed(false);
+							return request;
+						}							
+					}
+				}
+			}				
+			
+		} catch (IndexException e) {
+			//Index Exception while searching
+			logger.error(e.getMessage());
+		} catch (IllegalArgumentException e) {
+			//Invalid Arguement while searching
+			logger.info(e.getMessage());
+		}
+		return request;
+	}
+
+	
+}
