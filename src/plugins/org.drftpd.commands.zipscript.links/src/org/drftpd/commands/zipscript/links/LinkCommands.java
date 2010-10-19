fc600049807df14744c3c51d0777dbfe0a65d62d
cyber1331 <cyber1331>
2010-10-19 01:27:34 +0000

    Added Zip Script Links.  Works with rescan and fixlinks, also will add/remove links on upload/delete - This fixes #338

--- src/plugins/org.drftpd.commands.zipscript.links/src/org/drftpd/commands/zipscript/links/LinkCommands.java
+++ src/plugins/org.drftpd.commands.zipscript.links/src/org/drftpd/commands/zipscript/links/LinkCommands.java
@@ -19,11 +19,17 @@ package org.drftpd.commands.zipscript.links;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.bushe.swing.event.annotation.AnnotationProcessor;
+import org.bushe.swing.event.annotation.EventSubscriber;
 import org.drftpd.GlobalContext;
 import org.drftpd.commandmanager.CommandInterface;
 import org.drftpd.commandmanager.CommandRequest;
@@ -32,9 +38,13 @@ import org.drftpd.commandmanager.ImproperUsageException;
 import org.drftpd.commandmanager.StandardCommandManager;
 import org.drftpd.commands.zipscript.SFVStatus;
 import org.drftpd.commands.zipscript.vfs.ZipscriptVFSDataSFV;
+import org.drftpd.event.LoadPluginEvent;
+import org.drftpd.event.UnloadPluginEvent;
 import org.drftpd.exceptions.NoAvailableSlaveException;
 import org.drftpd.exceptions.SlaveUnavailableException;
 import org.drftpd.usermanager.User;
+import org.drftpd.util.CommonPluginUtils;
+import org.drftpd.util.MasterPluginUtils;
 import org.drftpd.vfs.DirectoryHandle;
 import org.drftpd.vfs.LinkHandle;
 import org.drftpd.vfs.ObjectNotValidException;
@@ -47,11 +57,33 @@ public class LinkCommands extends CommandInterface {
 
 	private static final Logger logger = Logger.getLogger(LinkCommands.class);
 
+	private ArrayList<FixLinksProcessDirInterface> _fixLinksAddons = new ArrayList<FixLinksProcessDirInterface>();
+	
 	private ResourceBundle _bundle;
+	
+	private StandardCommandManager _commandManager;
 
 	public void initialize(String method, String pluginName, StandardCommandManager cManager) {
     	super.initialize(method, pluginName, cManager);
 		_bundle = cManager.getResourceBundle();
+		
+		_commandManager = cManager;
+
+		// Subscribe to events
+		AnnotationProcessor.process(this);
+		
+		// Load any rescan post process providers from plugins
+		try {
+			List<FixLinksProcessDirInterface> loadedFixLinksAddons =
+				CommonPluginUtils.getPluginObjects(this, "org.drftpd.commands.zipscript.links", "FixLinksProcessDir", "Class");
+			for (FixLinksProcessDirInterface fixLinksAddon : loadedFixLinksAddons) {
+				fixLinksAddon.initialize(_commandManager);
+				_fixLinksAddons.add(fixLinksAddon);
+			}
+		} catch (IllegalArgumentException e) {
+			logger.error("Failed to load plugins for org.drftpd.commands.zipscript.links extension point 'FixLinksProcessDir'"+
+					", possibly the org.drftpd.commands.zipscript.links extension point definition has changed in the plugin.xml",e);
+		}	
 	}
 
 	public CommandResponse doSITE_FIXLINKS(CommandRequest request) throws ImproperUsageException {
@@ -114,7 +146,58 @@ public class LinkCommands extends CommandInterface {
 					// Slave holding sfv is unavailable
 				}
 			}
+			
+			for (FixLinksProcessDirInterface fixLinksAddon: _fixLinksAddons) {
+				CommandRequest workingDirReq = (CommandRequest) request.clone();
+				workingDirReq.setCurrentDirectory(workingDir);
+				workingDirReq.setUser(user.getName());
+				fixLinksAddon.processDir(workingDirReq);
+			}
+			
 		}
 		return response;
 	}
+	
+	@EventSubscriber @Override
+	public synchronized void onUnloadPluginEvent(UnloadPluginEvent event) {
+		super.onUnloadPluginEvent(event);
+		Set<FixLinksProcessDirInterface> unloadedFixLinksAddons =
+			MasterPluginUtils.getUnloadedExtensionObjects(this, "FixLinksProcessDir", event, _fixLinksAddons);
+		if (!unloadedFixLinksAddons.isEmpty()) {
+			ArrayList<FixLinksProcessDirInterface> clonedFixLinksAddons = new ArrayList<FixLinksProcessDirInterface>(_fixLinksAddons);
+			boolean addonRemoved = false;
+			for (Iterator<FixLinksProcessDirInterface> iter = clonedFixLinksAddons.iterator(); iter.hasNext();) {
+				FixLinksProcessDirInterface fixLinksAddon = iter.next();
+				if (unloadedFixLinksAddons.contains(fixLinksAddon)) {
+					logger.debug("Unloading fix links process addon provided by plugin "
+							+CommonPluginUtils.getPluginIdForObject(fixLinksAddon));
+					iter.remove();
+					addonRemoved = true;
+				}
+			}
+			if (addonRemoved) {
+				_fixLinksAddons = clonedFixLinksAddons;
+			}
+		}
+	}
+
+	@EventSubscriber
+	public synchronized void onLoadPluginEvent(LoadPluginEvent event) {
+		try {
+			List<FixLinksProcessDirInterface> loadedFixLinksAddons =
+				MasterPluginUtils.getLoadedExtensionObjects(this, "org.drftpd.commands.zipscript.links", "FixLinksProcessDir", "Class", event);
+			if (!loadedFixLinksAddons.isEmpty()) {
+				ArrayList<FixLinksProcessDirInterface> clonedFixLinksAddons = new ArrayList<FixLinksProcessDirInterface>(_fixLinksAddons);
+				for (FixLinksProcessDirInterface fixLinksAddon : loadedFixLinksAddons) {
+					fixLinksAddon.initialize(_commandManager);
+					clonedFixLinksAddons.add(fixLinksAddon);
+				}
+				_fixLinksAddons = clonedFixLinksAddons;
+			}
+		} catch (IllegalArgumentException e) {
+			logger.error("Failed to load plugins for org.drftpd.commands.zipscript.links extension point 'FixLinksProcessDir'"+
+					", possibly the org.drftpd.commands.zipscript.links extension point definition has changed in the plugin.xml",e);
+		}
+	}	
+	
 }
---
+++ src/plugins/org.drftpd.commands.zipscript.zip.links/src/org/drftpd/commands/zipscript/zip/links/hooks/LinksZipPostHook.java
@@ -0,0 +1,108 @@
+/*
+ * This file is part of DrFTPD, Distributed FTP Daemon.
+ *
+ * DrFTPD is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * DrFTPD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DrFTPD; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.drftpd.commands.zipscript.zip.links.hooks;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+import org.drftpd.commandmanager.CommandRequest;
+import org.drftpd.commandmanager.CommandResponse;
+import org.drftpd.commandmanager.PostHookInterface;
+import org.drftpd.commandmanager.StandardCommandManager;
+import org.drftpd.commands.zipscript.links.LinkUtils;
+import org.drftpd.commands.zipscript.vfs.ZipscriptVFSDataSFV;
+import org.drftpd.commands.zipscript.zip.DizStatus;
+import org.drftpd.commands.zipscript.zip.vfs.ZipscriptVFSDataZip;
+import org.drftpd.exceptions.NoAvailableSlaveException;
+import org.drftpd.exceptions.SlaveUnavailableException;
+
+/**
+ * @author CyBeR
+ * @version $Id: LinksZipPostHook.java 2076 2010-09-19 18:31:19Z djb61 $
+ */
+public class LinksZipPostHook implements PostHookInterface {
+
+	private ResourceBundle _bundle;
+
+	public void initialize(StandardCommandManager cManager) {
+		_bundle = cManager.getResourceBundle();
+	}
+
+	public void doLinksZipSTORIncompleteHook(CommandRequest request, CommandResponse response) {
+		if (response.getCode() != 226) {
+			// STOR failed, abort link
+			return;
+		}
+
+		ZipscriptVFSDataZip zipData = new ZipscriptVFSDataZip(request.getCurrentDirectory());
+		try {
+			DizStatus dizStatus = zipData.getDizStatus();
+			if (dizStatus.isFinished()) {
+				// dir is complete, remove link
+				LinkUtils.processLink(request, "delete", _bundle);
+			} else {
+				LinkUtils.processLink(request, "create", _bundle);
+			}
+		} catch (NoAvailableSlaveException e) {
+			// Slave holding zip is unavailable
+		} catch (FileNotFoundException e) {
+			// No zip in dir
+		} catch (IOException e) {
+			// zip not readable
+		}
+		return;
+	}
+
+	public void doLinksZipDELECleanupHook(CommandRequest request, CommandResponse response) {
+		if (response.getCode() != 250) {
+			// DELE failed, abort cleanup
+			return;
+		}
+
+		ZipscriptVFSDataZip zipData = new ZipscriptVFSDataZip(request.getCurrentDirectory());
+		try {
+			DizStatus dizStatus = zipData.getDizStatus();
+			if (!dizStatus.isFinished()) {
+				// dir is now incomplete, add link
+				LinkUtils.processLink(request, "create", _bundle);
+			}
+		} catch (NoAvailableSlaveException e) {
+			// Slave holding zip is unavailable
+		} catch (FileNotFoundException e) {
+			// No zip in dir
+			// We have to make sure there is a .sfv in this dir before deleting
+			try {
+				ZipscriptVFSDataSFV sfvData = new ZipscriptVFSDataSFV(request.getCurrentDirectory());
+				sfvData.getSFVStatus();
+			} catch (NoAvailableSlaveException e1) {
+				// Slave holding sfv is unavailable
+			} catch (FileNotFoundException e1) {
+				// No SFV in dir - now we can delete link
+				LinkUtils.processLink(request, "delete", _bundle);
+			} catch (IOException e1) {
+				// SFV not readable
+			} catch (SlaveUnavailableException e1) {
+				// No Slave with SFV available
+			}			
+		} catch (IOException e) {
+			// zip not readable
+		}
+		return;
+	}
+}
