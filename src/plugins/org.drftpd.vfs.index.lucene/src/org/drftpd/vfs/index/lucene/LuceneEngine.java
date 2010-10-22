07e28af960cd4b6a7cf29124357a834306c19b6e
djb61 <djb61>
2010-10-22 21:02:56 +0000

    If an exception occurs during lucene index rebuild ensure that the stacktrace is logged.

--- src/plugins/org.drftpd.vfs.index.lucene/src/org/drftpd/vfs/index/lucene/LuceneEngine.java
+++ src/plugins/org.drftpd.vfs.index.lucene/src/org/drftpd/vfs/index/lucene/LuceneEngine.java
@@ -470,6 +470,7 @@ public class LuceneEngine implements IndexEngineInterface {
 			recurseAndBuild(GlobalContext.getGlobalContext().getRoot());
 			commit(); // commit the writer so that the searcher can see the new stuff.
 		} catch (FileNotFoundException e) {
+			logger.warn("Exception whilst rebuilding lucene index",e);
 			throw new RuntimeException(e);
 		} finally {
 			_rebuilding = false;
