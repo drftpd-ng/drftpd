a90e2119a6b785e7fa91a2db9b0751b3274a65bc
erich9 <erich9>
2010-11-24 23:22:20 +0000

    Fix from k2r for #265 where drftpd.key is missing in slave.zip. Thanks again k2r!

--- genkey.bat
+++ genkey.bat
@@ -1,1 +1,2 @@
 keytool -genkey -alias drftpd -dname CN=drftpd -keypass drftpd -keystore drftpd.key -storepass drftpd
+copy /y drftpd.key %CD%\src\slave\resources\ 
--- genkey.sh
+++ genkey.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 keytool -genkey -alias drftpd -dname CN=drftpd -keypass drftpd -keystore drftpd.key -storepass drftpd -keyalg rsa "$@"
+cp ./drftpd.key ./src/slave/resources/
