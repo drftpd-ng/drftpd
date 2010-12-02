fe7212a1f771aad78952838e8bc1022ef1439f4c
djb61 <djb61>
2010-12-02 19:09:34 +0000

    Revert [2300] for the reasons detailed in the update to #265.

--- genkey.bat
+++ genkey.bat
@@ -1,2 +1,1 @@
 keytool -genkey -alias drftpd -dname CN=drftpd -keypass drftpd -keystore drftpd.key -storepass drftpd
-copy /y drftpd.key %CD%\src\slave\resources\ 
--- genkey.sh
+++ genkey.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 keytool -genkey -alias drftpd -dname CN=drftpd -keypass drftpd -keystore drftpd.key -storepass drftpd -keyalg rsa "$@"
-cp ./drftpd.key ./src/slave/resources/
