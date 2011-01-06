6392b5742dea4b6a508a7c0c3b7f0741b4d70433
cyber1331 <cyber1331>
2011-01-06 20:29:15 +0000

    Previous commit not needed as there is a VFS rename that was hard to find.

--- src/plugins/org.drftpd.commands.dir/src/org/drftpd/commands/dir/Dir.java
+++ src/plugins/org.drftpd.commands.dir/src/org/drftpd/commands/dir/Dir.java
@@ -33,7 +33,6 @@ import org.drftpd.commandmanager.ImproperUsageException;
 import org.drftpd.commandmanager.StandardCommandManager;
 import org.drftpd.dynamicdata.Key;
 import org.drftpd.event.DirectoryFtpEvent;
-import org.drftpd.event.RenameFtpEvent;
 import org.drftpd.exceptions.FileExistsException;
 import org.drftpd.exceptions.NoAvailableSlaveException;
 import org.drftpd.io.PermissionDeniedException;
@@ -492,8 +491,6 @@ public class Dir extends CommandInterface {
 		}
 
 		request.getSession().setObject(RENAMETO, toInode);
-
-		GlobalContext.getEventService().publishAsync(new RenameFtpEvent(request.getSession().getUserNull(request.getUser()), "RNTO", fromInode, toInode, System.currentTimeMillis()));
 		
 		/*logger.debug("after rename toInode-" +toInode);
 		logger.debug("after rename toInode.getPath()-" + toInode.getPath());
