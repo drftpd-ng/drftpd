1364128653889436c04ba8d025a426813d9c7c5f
Scitz0 <ola.ahlman@gmail.com>
2016-01-17 18:33:29 +0100

    Split new speedtest.net into its own plugin

---
+++ src/plugins/org.drftpd.commands.speedtest.net/src/org/drftpd/commands/speedtest/net/SlaveLocation.java
@@ -0,0 +1,50 @@
+/*
+ * This file is part of DrFTPD, Distributed FTP Daemon.
+ *
+ * DrFTPD is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * DrFTPD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DrFTPD; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.drftpd.commands.speedtest.net;
+
+/**
+ * @author scitz0
+ */
+public class SlaveLocation {
+
+	double latitude = 0;
+	double longitude = 0;
+
+	public SlaveLocation() {}
+
+	public SlaveLocation(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+}
