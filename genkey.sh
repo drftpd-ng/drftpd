ccd7d4165757772b941dc304692aa7008934f603
root <root@c-2387e355.bredbandsbolaget.se>
2016-04-09 10:18:36 +0200

    Update genkey.sh to generate an ecdsa by default. Add missing configuration options for serverstatus plugin

--- genkey.sh
+++ genkey.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-keytool -genkey -alias drftpd -dname CN=drftpd -keypass drftpd -keystore drftpd.key -storepass drftpd -keyalg rsa "$@"
+keytool -genkeypair -alias drftpd -keyalg EC -keysize 521 -validity 365 -storetype JKS -keystore drftpd.key -storepass drftpd
+#keytool -genkey -alias drftpd -dname CN=drftpd -keypass drftpd -keystore drftpd.key -storepass drftpd -keyalg rsa "$@"
