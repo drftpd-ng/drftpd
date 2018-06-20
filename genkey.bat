efc27b5b8b5cad74c0091b9fa6aeafd8bc3f6231
zc0nf <10996782+zc0nf@users.noreply.github.com>
2018-06-20 23:23:03 +0200

    FIX: The JKS keystore uses a proprietary format. Switch to PCKS12
    Upgrade JKS to PKCS12 :
    keytool -importkeystore -srckeystore drftpd3\userdata\drftpd.key -destkeystore drftpd3\userdata\drftpd.key -deststoretype pkcs12".

--- genkey.bat
+++ genkey.bat
@@ -15,5 +15,5 @@ if not exist "%USERDATA_SLAVE%" (
     goto :eof
   )
 )
-keytool -genkeypair -keyalg EC -keysize 256 -sigalg SHA256withECDSA -alias drftpd -dname CN=drftpd -keypass drftpd -keystore "%USERDATA_MASTER%drftpd.key" -storepass drftpd
+keytool -genkeypair -keyalg EC -keysize 256 -sigalg SHA256withECDSA -alias drftpd -dname CN=drftpd -keypass drftpd -keystore "%USERDATA_MASTER%drftpd.key" -storetype pkcs12 -storepass drftpd
 copy /Y "%USERDATA_MASTER%drftpd.key" "%USERDATA_SLAVE%"
