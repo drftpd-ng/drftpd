8fa7d415651597bba7e060b3b81d197e05a08993
zc0nf <10996782+zc0nf@users.noreply.github.com>
2019-01-12 12:01:47 +0100

    Revert GenKey #79

--- genkey.sh
+++ genkey.sh
@@ -4,6 +4,4 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in
 SCRIPTPATH=$(dirname "$SCRIPT")
 mkdir -p "$SCRIPTPATH/userdata"
-mkdir -p "$SCRIPTPATH/src/slave/resources/userdata"
 keytool -genkeypair -keyalg EC -keysize 256 -sigalg SHA256withECDSA -alias drftpd -dname CN=drftpd -keypass drftpd -keystore "$SCRIPTPATH/userdata/drftpd.key" -storetype pkcs12 -storepass drftpd "$@"
-cp "$SCRIPTPATH/userdata/drftpd.key" "$SCRIPTPATH/src/slave/resources/userdata/"
