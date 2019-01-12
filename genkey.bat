0f10b556ea91eb772f0c2e946f63b999731b291d
zc0nf <10996782+zc0nf@users.noreply.github.com>
2019-01-12 12:01:47 +0100

    Revert GenKey #79

--- genkey.bat
+++ genkey.bat
@@ -1,19 +1,10 @@
 set ROOT=%~dp0
-set USERDATA_MASTER=%ROOT%userdata\
-set USERDATA_SLAVE=%ROOT%src\slave\resources\userdata\
-if not exist "%USERDATA_MASTER%" (
-  mkdir "%USERDATA_MASTER%"
+set USERDATA=%ROOT%userdata\
+if not exist "%USERDATA%" (
+  mkdir "%USERDATA%"
   if "%errorlevel%" NEQ "0" (
-    echo Error while creating userdata folder %USERDATA_MASTER%
+    echo Error while creating userdata folder %USERDATA%
     goto :eof
   )
 )
-if not exist "%USERDATA_SLAVE%" (
-  mkdir "%USERDATA_SLAVE%"
-  if "%errorlevel%" NEQ "0" (
-    echo Error while creating userdata folder %USERDATA_SLAVE%
-    goto :eof
-  )
-)
-keytool -genkeypair -keyalg EC -keysize 256 -sigalg SHA256withECDSA -alias drftpd -dname CN=drftpd -keypass drftpd -keystore "%USERDATA_MASTER%drftpd.key" -storetype pkcs12 -storepass drftpd
-copy /Y "%USERDATA_MASTER%drftpd.key" "%USERDATA_SLAVE%"
+keytool -genkeypair -keyalg EC -keysize 256 -sigalg SHA256withECDSA -alias drftpd -dname CN=drftpd -keypass drftpd -keystore "%USERDATA%drftpd.key" -storetype pkcs12 -storepass drftpd
