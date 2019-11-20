57c6fc5f792ee475d6576a2411bb25c18c28237b
zc0nf <10996782+zc0nf@users.noreply.github.com>
2019-11-20 23:32:03 +0100

    FIX: newInstance are deprecated

--- src/common/src/org/drftpd/util/CommonPluginUtils.java
+++ src/common/src/org/drftpd/util/CommonPluginUtils.java
@@ -255,7 +255,7 @@ public class CommonPluginUtils {
 				Class<T> pluginCls = loadPluginClass(pluginLoader, 
 						plugin.getParameter(classParamName).valueAsString());
 				if (constructorSig == null) {
-					pluginObjs.add(pluginCls.newInstance());
+					pluginObjs.add(pluginCls.getDeclaredConstructor().newInstance());
 				} else {
 					pluginObjs.add(pluginCls.getConstructor(constructorSig).newInstance(constructorArgs));
 				}
@@ -510,7 +510,7 @@ public class CommonPluginUtils {
 					Class<T> pluginCls = loadPluginClass(pluginLoader, 
 							plugin.getParameter(classParamName).valueAsString());
 					if (constructorSig == null) {
-						return pluginCls.newInstance();
+						return pluginCls.getDeclaredConstructor().newInstance();
 					}
 					return pluginCls.getConstructor(constructorSig).newInstance(constructorArgs);
 				}
@@ -1077,7 +1077,7 @@ public class CommonPluginUtils {
 				if (createInstance) {
 					T pluginInstance = null;
 					if (constructorSig == null) {
-						pluginInstance = pluginCls.newInstance();
+						pluginInstance = pluginCls.getDeclaredConstructor().newInstance();
 					} else {
 						pluginInstance = pluginCls.getConstructor(constructorSig).newInstance(constructorArgs);
 					}
@@ -1316,7 +1316,7 @@ public class CommonPluginUtils {
 					if (createInstance) {
 						T pluginInstance = null;
 						if (constructorSig == null) {
-							pluginInstance = pluginCls.newInstance();
+							pluginInstance = pluginCls.getDeclaredConstructor().newInstance();
 						} else {
 							pluginInstance = pluginCls.getConstructor(constructorSig).newInstance(constructorArgs);
 						}
