0ec9b9f25575f1c9f223340ef5f87a9ebe712feb
k2r <k2r.contact@gmail.com>
2020-03-18 19:51:10 +0100

    Introduce maven modules

---
+++ core/src/main/java/org/drftpd/plugins/commandmanager/CommandInterface.java
@@ -0,0 +1,182 @@
+/*
+ * This file is part of DrFTPD, Distributed FTP Daemon.
+ *
+ * DrFTPD is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * DrFTPD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DrFTPD; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.drftpd.plugins.commandmanager;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bushe.swing.event.annotation.AnnotationProcessor;
+import org.bushe.swing.event.annotation.EventSubscriber;
+import org.drftpd.common.CommandHook;
+import org.drftpd.common.HookType;
+import org.drftpd.master.GlobalContext;
+import org.drftpd.master.commandmanager.CommandRequestInterface;
+import org.drftpd.master.commandmanager.CommandResponseInterface;
+import org.drftpd.master.permissions.Permission;
+import org.drftpd.master.usermanager.NoSuchUserException;
+import org.drftpd.master.usermanager.User;
+import org.drftpd.master.usermanager.UserFileException;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+
+import java.io.*;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author djb61
+ * @version $Id$
+ */
+public abstract class CommandInterface {
+
+	private static final Logger logger = LogManager.getLogger(CommandInterface.class);
+
+	protected String[] _featReplies;
+
+	private Multimap<Integer, HookContainer<PostHookInterface>> _postHooks;
+
+	private Multimap<Integer, HookContainer<PreHookInterface>> _preHooks;
+
+	public CommandInterface() {
+		// Subscribe to events
+		AnnotationProcessor.process(this);
+	}
+
+	public synchronized void initialize(String method, String pluginName, StandardCommandManager cManager) {
+		Multimap<Integer, HookContainer<PostHookInterface>> postHooks = MultimapBuilder.treeKeys().linkedListValues().build();
+		Multimap<Integer, HookContainer<PreHookInterface>> preHooks = MultimapBuilder.treeKeys().linkedListValues().build();
+		Set<Method> hooksMethods = GlobalContext.getHooksMethods();
+		// TODO [DONE] @JRI Plug hooks
+		try {
+			for (Method annotatedMethod : hooksMethods) {
+				Class<?> declaringClass = annotatedMethod.getDeclaringClass();
+				CommandHook annotation = annotatedMethod.getAnnotation(CommandHook.class);
+				int priority = annotation.priority();
+				String[] commands = annotation.commands();
+				boolean handleClass = Arrays.stream(commands).anyMatch(method::equals);
+				if (!handleClass) continue;
+
+				Object hookClass = declaringClass.getConstructor().newInstance();
+				HookType type = annotation.type();
+				if (type.equals(HookType.PRE)) {
+					preHooks.put(priority, new HookContainer(annotatedMethod, hookClass));
+				} else {
+					postHooks.put(priority, new HookContainer(annotatedMethod, hookClass));
+				}
+			}
+		} catch (Exception e) {
+			logger.error("Failed to load plugins for {} extension point 'PreHook', possibly the {} extension point definition has changed in the plugin.xml", pluginName, pluginName, e);
+		}
+		_preHooks = preHooks;
+		_postHooks = postHooks;
+	}
+
+	protected void doPostHooks(CommandRequestInterface request, CommandResponseInterface response) {
+		for (HookContainer<PostHookInterface> hook : _postHooks.values()) {
+			Method m = hook.getMethod();
+			try {
+				m.invoke(hook.getHookInterfaceInstance(), request, response);
+			}
+			catch (Exception e) {
+                logger.error("Error while loading/invoking posthook {}", m.toString(), e.getCause());
+				/* Not that important, this just means that this post hook
+				 * failed and we'll just move onto the next one
+				 */
+			}
+		}
+	}
+
+	protected CommandRequestInterface doPreHooks(CommandRequestInterface request) {
+		request.setAllowed(true);
+		for (HookContainer<PreHookInterface> hook : _preHooks.values()) {
+			Method m = hook.getMethod();
+			try {
+				request = (CommandRequestInterface) m.invoke(hook.getHookInterfaceInstance(), new Object[] {request});
+			}
+			catch (Exception e) {
+                logger.error("Error while loading/invoking prehook {}", m.toString(), e.getCause());
+				/* Not that important, this just means that this pre hook
+				 * failed and we'll just move onto the next one
+				 */
+			}
+		}
+		return request;
+	}
+
+	protected User getUserObject(String user) throws NoSuchUserException, UserFileException {
+		return GlobalContext.getGlobalContext().getUserManager().getUserByName(user);
+	}
+
+	public String[] getFeatReplies() {
+		return _featReplies;
+	}
+
+	public void addTextToResponse(CommandResponse response, String file) throws IOException {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.ISO_8859_1));
+			response.addComment(reader);
+			reader.close();
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+	}
+
+	protected boolean checkCustomPermissionWithPrimaryGroup(User targetUser, CommandRequest request, String permissionName, String defaultPermission) {
+		if (checkCustomPermission(request, permissionName, defaultPermission)) {
+			return false;
+		}
+		try {
+			return targetUser.getGroup().equals(request.getUserObject().getGroup());
+		} catch (NoSuchUserException | UserFileException e) {
+			logger.warn("",e);
+			return false;
+		}
+	}
+
+	protected boolean checkCustomPermission(CommandRequest request, String permissionName,
+			String defaultPermission) {
+		String permissionString = request.getProperties().getProperty(permissionName,defaultPermission);
+		User user;
+		try {
+			user = request.getUserObject();
+		} catch (NoSuchUserException e) {
+			logger.warn("",e);
+			return false;
+		} catch (UserFileException e) {
+			logger.warn("",e);
+			return false;
+		}
+		return new Permission(permissionString).check(user);
+	}
+
+	/**
+	 * Called when the command instance has been unloaded from the parent command map. At this
+	 * point the command is no longer referenced or accessible, this method performs any cleanup
+	 * required at this point.
+	 */
+	protected void unload() {
+		AnnotationProcessor.unprocess(this);
+	}
+}
---
+++ imdb/src/main/java/org/drftpd/commands/imdb/find/IMDBAction.java
@@ -0,0 +1,86 @@
+/*
+ * This file is part of DrFTPD, Distributed FTP Daemon.
+ *
+ * DrFTPD is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * DrFTPD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DrFTPD; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.drftpd.commands.imdb.find;
+
+import org.apache.commons.text.WordUtils;
+import org.drftpd.commands.find.action.ActionInterface;
+import org.drftpd.commands.imdb.vfs.IMDBVFSDataNFO;
+import org.drftpd.master.vfs.DirectoryHandle;
+import org.drftpd.master.vfs.InodeHandle;
+import org.drftpd.plugins.commandmanager.CommandRequest;
+import org.drftpd.plugins.commandmanager.ImproperUsageException;
+import org.drftpd.protocol.imdb.common.IMDBInfo;
+
+/**
+ * @author scitz0
+ * @version $Id: NukeAction.java 2482 2011-06-28 10:20:44Z scitz0 $
+ */
+public class IMDBAction implements ActionInterface {
+    private boolean _failed;
+
+    @Override
+    public String name() {
+        return "PrintIMDB";
+    }
+
+    @Override
+    public void initialize(String action, String[] args) throws ImproperUsageException {
+    }
+
+    @Override
+    public String exec(CommandRequest request, InodeHandle inode) {
+        _failed = false;
+        IMDBVFSDataNFO imdbData = new IMDBVFSDataNFO((DirectoryHandle) inode);
+        IMDBInfo imdbInfo = imdbData.getIMDBInfoFromCache();
+        if (imdbInfo != null) {
+            if (imdbInfo.getMovieFound()) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("#########################################").append(")\n");
+                sb.append("# Title # - ").append(imdbInfo.getTitle()).append("\n");
+                sb.append("# Year # - ").append(imdbInfo.getYear()).append("\n");
+                sb.append("# Runtime # - ").append(imdbInfo.getRuntime()).append(" min").append("\n");
+                sb.append("# Language # - ").append(imdbInfo.getLanguage()).append("\n");
+                sb.append("# Country # - ").append(imdbInfo.getCountry()).append("\n");
+                sb.append("# Director # - ").append(imdbInfo.getDirector()).append("\n");
+                sb.append("# Genres # - ").append(imdbInfo.getGenres()).append("\n");
+                sb.append("# Plot #\n").append(WordUtils.wrap(imdbInfo.getPlot(), 70));
+                sb.append("# Rating # - ");
+                sb.append(imdbInfo.getRating() != null ? imdbInfo.getRating() / 10 + "." + imdbInfo.getRating() % 10 + "/10" : "-").append("\n");
+                sb.append("# Votes # - ").append(imdbInfo.getVotes()).append("\n");
+                sb.append("# URL # - ").append(imdbInfo.getURL()).append("\n");
+                return sb.toString();
+            }
+        }
+        return "#########################################\nNO IMDB INFO FOUND FOR: " + inode.getPath();
+    }
+
+    @Override
+    public boolean execInDirs() {
+        return true;
+    }
+
+    @Override
+    public boolean execInFiles() {
+        return false;
+    }
+
+    @Override
+    public boolean failed() {
+        return _failed;
+    }
+}
