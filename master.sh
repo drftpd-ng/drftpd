9e247078ef11d112d67bd01a3187b4ddad8a6e90
k2r <k2r.contact@gmail.com>
2020-03-25 23:47:47 +0100

    Specify log config file

--- master.sh
+++ master.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Xms1024M -classpath "build/*" org.drftpd.MasterBoot
+java -Xms1024M -classpath "build/*" -Dlog4j.configurationFile=config/log4j2-master.xml org.drftpd.MasterBoot
--- slave.sh
+++ slave.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Xms1024M -classpath "build/*" org.drftpd.SlaveBoot
+java -Xms1024M -classpath "build/*" -Dlog4j.configurationFile=config/log4j2-slave.xml org.drftpd.SlaveBoot
