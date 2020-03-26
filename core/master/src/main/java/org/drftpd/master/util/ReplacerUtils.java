d56ff058648a6f3948374560f210cd86abbc2b27
k2r <k2r.contact@gmail.com>
2020-03-26 14:14:47 +0100

    Handle tanesha format option in the new system

--- core/master/src/main/java/org/drftpd/master/util/ReplacerUtils.java
+++ core/master/src/main/java/org/drftpd/master/util/ReplacerUtils.java
@@ -17,12 +17,17 @@
  */
 package org.drftpd.master.util;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.StringSubstitutor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author mog
@@ -31,21 +36,52 @@ import java.util.ResourceBundle;
 public class ReplacerUtils {
 
     private static final Logger logger = LogManager.getLogger(ReplacerUtils.class);
+    private static final Pattern pattern = Pattern.compile("\\$\\{([a-zA-Z0-9\\.,-]+)\\}");
 
     private ReplacerUtils() {
         super();
     }
 
     public static String jprintf(String template, Map<String, Object> env) {
-        StringSubstitutor sub = new StringSubstitutor(env);
+        Map<String, Object> envVars = new HashMap<>(env);
+        Matcher matcher = pattern.matcher(template);
+        String[] variables = matcher.results().map(m -> m.group(1)).toArray(String[]::new);
+        // Take care of all options
+        for (String variable : variables) {
+            if (variable.contains(",")) {
+                String[] varSplitter = variable.split(",");
+                String varName = varSplitter[0];
+                String currentValue = envVars.get(varName).toString();
+                String options = varSplitter.length == 2 ? varSplitter[1] : null;
+                if (options != null) {
+                    boolean alignLeft = options.startsWith("-");
+                    options = options.replace("-", "");
+                    String[] sizeAndMaxOptions = options.split("\\.");
+                    int size = Integer.parseInt(sizeAndMaxOptions[0]);
+                    Integer maxSize = sizeAndMaxOptions.length == 2 ? Integer.parseInt(sizeAndMaxOptions[1]) : null;
+                    // Adapt from the option
+                    int paddingSize = size - currentValue.length();
+                    if (alignLeft) {
+                        currentValue = StringUtils.rightPad(currentValue, paddingSize);
+                    } else {
+                        currentValue = StringUtils.leftPad(currentValue, paddingSize);
+                    }
+                    if (maxSize != null && currentValue.length() > maxSize) {
+                        currentValue = alignLeft ? currentValue.substring(0, maxSize) :
+                                currentValue.substring(currentValue.length() - maxSize);
+                    }
+                }
+                envVars.put(variable, currentValue);
+            }
+        }
+        StringSubstitutor sub = new StringSubstitutor(envVars);
         return sub.replace(template);
     }
 
     public static String jprintf(String key, Map<String, Object> env, ResourceBundle bundle) {
         try {
             String template = bundle.getString(key);
-            StringSubstitutor sub = new StringSubstitutor(env);
-            return sub.replace(template);
+            return jprintf(template, env);
         } catch (Exception e) {
             logger.info("Error formatting message for key - {}", key, e);
             return key;
---
+++ core/master/src/test/java/org/drftpd/master/util/ReplacerUtilsTest.java
@@ -0,0 +1,28 @@
+package org.drftpd.master.util;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.drftpd.master.util.ReplacerUtils.jprintf;
+
+public class ReplacerUtilsTest extends TestCase {
+    public void testVarReplacement() {
+        Map<String, Object> env = new HashMap<>();
+        env.put("position", 3);
+        env.put("user", "test");
+        env.put("group", "sysop");
+        env.put("files", 11);
+        env.put("bytes", "10To");
+        env.put("speed", "27.4MB/s");
+        env.put("percent", "45");
+        // Complex template
+        String template = "#${position,3.3} ${user,-13} @ ${group,-11} - ${files,7}F - ${bytes,7} - ${speed,10} - ${percent,6}";
+        Assert.assertEquals("# 3 test      @ sysop  -    11F - 10To - 27.4MB/s -   45", jprintf(template, env));
+        // Simple template
+        template = "#${position} ${user} @ ${group} - ${files}F - ${bytes} - ${speed} - ${percent}";
+        Assert.assertEquals("#3 test @ sysop - 11F - 10To - 27.4MB/s - 45", jprintf(template, env));
+    }
+}
