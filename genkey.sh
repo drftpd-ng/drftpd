a1893149fbc17b906defde202713da0f240b1402
zc0nf <10996782+zc0nf@users.noreply.github.com>
2020-03-28 13:36:21 +0100

    FIX: Deprecated keytool -keysize
    Changes :
    - keysize to groupname
    - 256 to secp384r1
    - sigalg to default
    - validity 90 to 365 days

--- genkey.sh
+++ genkey.sh
@@ -4,4 +4,4 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in
 SCRIPTPATH=$(dirname "$SCRIPT")
 mkdir -p "$SCRIPTPATH/userdata"
-keytool -genkeypair -keyalg EC -keysize 256 -sigalg SHA256withECDSA -alias drftpd -dname CN=drftpd -keypass drftpd -keystore "$SCRIPTPATH/userdata/drftpd.key" -storetype pkcs12 -storepass drftpd "$@"
+keytool -genkeypair -keyalg EC -groupname secp384r1 -alias drftpd -dname CN=drftpd -keypass drftpd -keystore "$SCRIPTPATH/userdata/drftpd.key" -storetype pkcs12 -storepass drftpd -validity 365 "$@"
