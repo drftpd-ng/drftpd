fe6e579d305c84bc018ff839a4b7ed1d07d300cf
Epochyx <epochyx@tutanota.com>
2020-03-29 03:56:08 +0000

    add proper permissions on launching scripts and use proper delimiter for classpath

--- slave.sh
+++ slave.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Xms1024M -classpath "dependencies/*;build/*" -Dlog4j.configurationFile=config/log4j2-slave.xml org.drftpd.SlaveBoot
+java -Xms1024M -classpath "dependencies/*:build/*" -Dlog4j.configurationFile=config/log4j2-slave.xml org.drftpd.SlaveBoot
