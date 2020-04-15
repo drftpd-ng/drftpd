6ed817a9d5a8c1f44d63f7013082b87391a02763
Mike van Goor <mike@probie.nl>
2020-04-15 11:08:08 +0200

    Correct java class to start

--- src/core/master/src/main/resources/master/master.sh
+++ src/core/master/src/main/resources/master/master.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Xms1024M -classpath "lib/*:build/*" -Dlog4j.configurationFile=config/log4j2-master.xml org.drftpd.Master
+java -Xms1024M -classpath "lib/*:build/*" -Dlog4j.configurationFile=config/log4j2-master.xml org.drftpd.mastrer.Master
