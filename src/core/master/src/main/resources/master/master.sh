f9d988c24e75e138436b4221a95e785719d7ee05
Mike van Goor <mvangoor@schubergphilis.com>
2020-05-25 22:44:29 +0200

    Typo

--- src/core/master/src/main/resources/master/master.sh
+++ src/core/master/src/main/resources/master/master.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CLASSPATH="lib/*;build/*"
+CLASSPATH="lib/*:build/*"
 # Add JVM Options here however you see fit and please check if the max memory Xmx is good enough for your master
 JVM_OPTS="-Xmx3G -XX:+UseG1GC"
 OPTIONS="-Dlog4j.configurationFile=config/log4j2-master.xml"
--- src/core/slave/src/main/resources/slave/slave.sh
+++ src/core/slave/src/main/resources/slave/slave.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-CLASSPATH="lib/*;build/*"
+CLASSPATH="lib/*:build/*"
 # Add JVM Options here however you see fit and please check if the max memory Xmx is good enough for your master
 JVM_OPTS="-Xmx1G -XX:+UseG1GC"
 OPTIONS="-Dlog4j.configurationFile=config/log4j2-slave.xml"
 PROGRAM="org.drftpd.slave.Slave"
 
-java ${JVM_OPTS} -classpath ${CLASSPATH} ${JVM_OPTS} ${OPTIONS} ${PROGRAM}
+java ${JVM_OPTS} -classpath ${CLASSPATH} ${OPTIONS} ${PROGRAM}
