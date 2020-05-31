d6d58c93365b46fb3634b6dd61d85b606fb5e122
zc0nf <10996782+zc0nf@users.noreply.github.com>
2020-06-01 01:18:12 +0200

    FIX: GPL v2

--- src/plugins/sitebot/src/main/java/org/drftpd/master/sitebot/blowfish/BlowfishCBC.java
+++ src/plugins/sitebot/src/main/java/org/drftpd/master/sitebot/blowfish/BlowfishCBC.java
@@ -1,12 +1,30 @@
 /*
+ * This file is part of DrFTPD, Distributed FTP Daemon.
+ *
+ * DrFTPD is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * DrFTPD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DrFTPD; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/*
  * BlowfishCBC.java version 1.00.00
  *
  * Code Written by k2r (k2r.contact@gmail.com)
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  */
 package org.drftpd.master.sitebot.blowfish;
 
