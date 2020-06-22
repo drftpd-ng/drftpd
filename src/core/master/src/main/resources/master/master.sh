566b77cb7774ad806302fe2a07df9da84ee06301
zc0nf <10996782+zc0nf@users.noreply.github.com>
2020-06-22 20:46:23 +0200

    Daemon example [linux]
    #154

--- src/core/master/src/main/resources/master/master.sh
+++ src/core/master/src/main/resources/master/master.sh
@@ -1,4 +1,44 @@
 #!/bin/sh
+#
+# This file is part of DrFTPD, Distributed FTP Daemon.
+#
+# DrFTPD is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# DrFTPD is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DrFTPD; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+####################################################################################################
+#                                                                                                  #
+# DrFTPD service example:                                                                          #
+#                                                                                                  #
+# nano /etc/systemd/system/master.service                                                          #
+#                                                                                                  #
+# [Unit]                                                                                           #
+# Description=DrFTPD Master                                                                        #
+# [Service]                                                                                        #
+# Type=simple                                                                                      #
+# User=drftpd-service                                                                              #
+# Group=drftpd-service                                                                             #
+# UMask=007                                                                                        #
+# WorkingDirectory=/home/drftpd-service/drftpd/runtime/master                                      #
+# RemainAfterExit=yes                                                                              #
+# ExecStart=/home/drftpd-service/drftpd/runtime/master/master.sh                                   #
+# [Install]                                                                                        #
+# WantedBy=multi-user.target                                                                       #
+#                                                                                                  #
+# systemctl enable master                                                                          #
+#                                                                                                  #
+####################################################################################################
 
 CLASSPATH="lib/*:build/*"
 # Add JVM Options here however you see fit and please check if the max memory Xmx is good enough for your master
@@ -6,4 +46,4 @@ JVM_OPTS="-Xmx3G -XX:+UseG1GC"
 OPTIONS="-Dlog4j.configurationFile=config/log4j2-master.xml"
 PROGRAM="org.drftpd.master.Master"
 
-java ${JVM_OPTS} -classpath ${CLASSPATH} ${JVM_OPTS} ${OPTIONS} ${PROGRAM}
+java ${JVM_OPTS} -classpath ${CLASSPATH} ${OPTIONS} ${PROGRAM}
--- src/core/slave/src/main/resources/slave/slave.sh
+++ src/core/slave/src/main/resources/slave/slave.sh
@@ -1,7 +1,47 @@
 #!/bin/sh
+#
+# This file is part of DrFTPD, Distributed FTP Daemon.
+#
+# DrFTPD is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# DrFTPD is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DrFTPD; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+####################################################################################################
+#                                                                                                  #
+# DrFTPD service example:                                                                          #
+#                                                                                                  #
+# nano /etc/systemd/system/slave.service                                                           #
+#                                                                                                  #
+# [Unit]                                                                                           #
+# Description=DrFTPD Slave                                                                         #
+# [Service]                                                                                        #
+# Type=simple                                                                                      #
+# User=drftpd-service                                                                              #
+# Group=drftpd-service                                                                             #
+# UMask=007                                                                                        #
+# WorkingDirectory=/home/drftpd-service/drftpd/runtime/slave                                       #
+# RemainAfterExit=yes                                                                              #
+# ExecStart=/home/drftpd-service/drftpd/runtime/slave/slave.sh                                     #
+# [Install]                                                                                        #
+# WantedBy=multi-user.target                                                                       #
+#                                                                                                  #
+# systemctl enable slave                                                                           #
+#                                                                                                  #
+####################################################################################################
 
 CLASSPATH="lib/*:build/*"
-# Add JVM Options here however you see fit and please check if the max memory Xmx is good enough for your master
+# Add JVM Options here however you see fit and please check if the max memory Xmx is good enough for your slave
 JVM_OPTS="-Xmx1G -XX:+UseG1GC"
 OPTIONS="-Dlog4j.configurationFile=config/log4j2-slave.xml"
 PROGRAM="org.drftpd.slave.Slave"
