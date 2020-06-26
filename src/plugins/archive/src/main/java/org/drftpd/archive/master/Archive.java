42da871e968c711d23e60efb3f668e95da99bd73
Mike van Goor <mvangoor@schubergphilis.com>
2020-06-26 09:52:13 +0200

    Annotation processing requires an argument

--- src/plugins/archive/src/main/java/org/drftpd/archive/master/Archive.java
+++ src/plugins/archive/src/main/java/org/drftpd/archive/master/Archive.java
@@ -228,7 +228,7 @@ public class Archive implements PluginInterface {
     }
 
     @EventSubscriber
-    public void onReloadEvent() {
+    public void onReloadEvent(ReloadEvent event) {
         logger.info("Received reload event, reloading");
         reload();
     }
