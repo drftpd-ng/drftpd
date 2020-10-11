dbcb2802b2fee1beca2a04fbd322bcfb77f9d681
zc0nf <10996782+zc0nf@users.noreply.github.com>
2020-10-11 16:30:16 +0200

    Garbage Collector: Switch to ZGC
    Links:
    - https://wiki.openjdk.java.net/display/zgc/Main
    - https://bugs.openjdk.java.net/browse/JDK-8209683

--- src/core/master/src/main/resources/master/master.bat
+++ src/core/master/src/main/resources/master/master.bat
@@ -1,6 +1,6 @@
 set CLASSPATH=lib/*;build/*
 rem Add JVM Options here however you see fit and please check if the max memory Xmx is good enough for your master
-set JVM_OPTS=-Xmx3G -XX:+UseG1GC
+set JVM_OPTS=-Xmx3G -XX:+UseZGC
 set OPTIONS=-Dlog4j.configurationFile=config/log4j2-master.xml
 set PROGRAM=org.drftpd.master.Master
 
