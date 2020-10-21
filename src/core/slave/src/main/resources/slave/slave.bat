a47b548708e6538f5c20341397ed7e2233d4b2cd
zc0nf <10996782+zc0nf@users.noreply.github.com>
2020-10-21 06:22:14 +0200

    FIX: ZGC for slave too!

--- src/core/slave/src/main/resources/slave/slave.bat
+++ src/core/slave/src/main/resources/slave/slave.bat
@@ -1,6 +1,6 @@
 set CLASSPATH=lib/*;build/*
 rem Add JVM Options here however you see fit and please check if the max memory Xmx is good enough for your master
-set JVM_OPTS=-Xmx1G -XX:+UseG1GC
+set JVM_OPTS=-Xmx1G -XX:+UseZGC
 set OPTIONS=-Dlog4j.configurationFile=config/log4j2-slave.xml
 set PROGRAM=org.drftpd.slave.Slave
 
