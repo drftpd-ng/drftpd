cfc0781885917d8eef183ed0ed488d86c768615a
Mike van Goor <mvangoor@schubergphilis.com>
2021-01-24 21:31:09 +0100

    Add a Command PRE Hook for "doSITE_DELSLAVE" to deny deletion if it is referenced in autofreespace configuration

---
+++ src/plugins/autofreespace/src/main/java/org/drftpd/autofreespace/master/AutoFreeSpaceHooks.java
@@ -0,0 +1,47 @@
+package org.drftpd.autofreespace.master;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.drftpd.common.extensibility.CommandHook;
+import org.drftpd.common.extensibility.HookType;
+import org.drftpd.master.GlobalContext;
+import org.drftpd.master.commands.CommandRequest;
+import org.drftpd.master.commands.CommandRequestInterface;
+import org.drftpd.master.commands.CommandResponse;
+import org.drftpd.master.slavemanagement.RemoteSlave;
+import org.drftpd.slave.exceptions.ObjectNotFoundException;
+
+import java.util.StringTokenizer;
+
+public class AutoFreeSpaceHooks {
+
+    protected static final Logger logger = LogManager.getLogger(AutoFreeSpaceHooks.class);
+
+    @CommandHook(commands = "doSITE_DELSLAVE", priority = 100, type = HookType.PRE)
+    public CommandRequestInterface doAutoFreeSpacePreHook(CommandRequest request) {
+        // First handle syntax errors cases which will be handled in the normal command execution
+        if (!request.hasArgument()) {
+            // Syntax error but we'll let the command itself deal with it
+            return request;
+        }
+        StringTokenizer arguments = new StringTokenizer(request.getArgument());
+        if (!arguments.hasMoreTokens()) {
+            // Syntax error but we'll let the command itself deal with it
+            return request;
+        }
+        String slavename = arguments.nextToken();
+        RemoteSlave slave;
+        try {
+            slave = GlobalContext.getGlobalContext().getSlaveManager().getRemoteSlave(slavename);
+        } catch(ObjectNotFoundException e) {
+            // This is an error, but we'll let the command itself deal with it
+            return request;
+        }
+
+        if (AutoFreeSpaceSettings.getSettings().getExcludeSlaves().contains(slave.getName())) {
+            request.setAllowed(false);
+            request.setDeniedResponse(new CommandResponse(550, "Slave "+slave.getName()+" is still referenced in AutoFreeSpace configuration, not allowing delete"));
+        }
+        return request;
+    }
+}
