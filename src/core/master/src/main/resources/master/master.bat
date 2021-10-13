6b44ba9da963d3f4183c2da06846bc747f50e568
zc0nf <10996782+zc0nf@users.noreply.github.com>
2021-10-13 14:32:25 +0200

    FIX: Same for Windows... Oops

--- src/core/master/src/main/resources/master/master.bat
+++ src/core/master/src/main/resources/master/master.bat
@@ -28,7 +28,7 @@ rem ############################################################################
 
 set CLASSPATH=lib/*;build/*
 rem Add JVM Options here however you see fit and please check if the max memory Xmx is good enough for your master
-set JVM_OPTS=-Xmx3G -XX:+UseZGC
+set JVM_OPTS=-Xmx3G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC
 set OPTIONS=-Dlog4j.configurationFile=config/log4j2-master.xml
 set PROGRAM=org.drftpd.master.Master
 
