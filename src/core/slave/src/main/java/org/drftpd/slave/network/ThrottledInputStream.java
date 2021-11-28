a35079d0411ae33bd9a952d9a41aa7db50623767
Mike van Goor <mike@probie.nl>
2021-11-28 13:06:07 +0100

    Re-work the throttledInputStream

--- src/core/slave/src/main/java/org/drftpd/slave/network/ThrottledInputStream.java
+++ src/core/slave/src/main/java/org/drftpd/slave/network/ThrottledInputStream.java
@@ -17,85 +17,77 @@
  */
 package org.drftpd.slave.network;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.io.InputStream;
 
 public class ThrottledInputStream extends InputStream {
-    private long _counter;
-    private final InputStream _in;
-    private long _lastCounter;
-    private long _lastMillis;
-    private long _maxBytesPerSecond;
-    private final Object _monitor;
 
-    public ThrottledInputStream(InputStream input, long maximumBytesPerSecond) {
-        _in = input;
-        setMaxBytesPerSecond(maximumBytesPerSecond);
-        _lastMillis = System.currentTimeMillis();
-        _monitor = new Object();
-    }
+    private static final Logger logger = LogManager.getLogger(ThrottledInputStream.class);
+
+    private final InputStream _in;
+
+    private final long _maxBytesPerSecond;
+    private final long _startTime = System.nanoTime();
+
+    private long _bytesRead = 0;
+    private long _totalSleepTime = 0;
+
+    private static final Object _monitor = new Object();
+    private static final long SLEEP_DURATION_MS = 30;
 
     public ThrottledInputStream(InputStream input) {
         this(input, Long.MAX_VALUE);
     }
 
+    public ThrottledInputStream(InputStream input, long maximumBytesPerSecond) {
+        assert(maximumBytesPerSecond >= 0);
+        assert(input != null);
+
+        this._in = input;
+        this._maxBytesPerSecond = maximumBytesPerSecond;
+        logger.debug("Initialized with maximum Bytes/second as {}", _maxBytesPerSecond);
+    }
+
+    /** @inheritDoc */
     @Override
     public void close() throws IOException {
         _in.close();
     }
 
-    public long getCounter() {
-        return _counter;
-    }
-
-    public void setCounter(long newValue) {
-        _counter = newValue;
-    }
-
+    /** @inheritDoc */
     @Override
     public int read() throws IOException {
+        throttle();
         int result = _in.read();
         if (result != -1) {
-            _counter++;
-            waitIfNecessary();
+            _bytesRead++;
         }
         return result;
     }
 
+    /** @inheritDoc */
     @Override
     public int read(byte[] b) throws IOException {
+        throttle();
         int result = _in.read(b);
-        if (result > 0) {
-            _counter += result;
-            waitIfNecessary();
+        if (result != -1) {
+            _bytesRead += result;
         }
         return result;
     }
 
+    /** @inheritDoc */
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
+        throttle();
         int result = _in.read(b, off, len);
-        if (result > 0) {
-            _counter += result;
-            waitIfNecessary();
+        if (result != -1) {
+            _bytesRead += result;
         }
         return result;
-    }
-
-    public void resetCounter() {
-        setCounter(0);
-    }
-
-    public void setMaxBytesPerSecond(long maxBytes) {
-        if (maxBytes < 0) {
-            throw new IllegalArgumentException("Maximum bytes per second must be at least one.");
-        }
-        _maxBytesPerSecond = maxBytes;
-    }
-
-    @Override
-    public long skip(long n) throws IOException {
-        return _in.skip(n);
     }
 
     public void wake() {
@@ -104,26 +96,58 @@ public class ThrottledInputStream extends InputStream {
         }
     }
 
-    private void waitIfNecessary() {
-        if (_maxBytesPerSecond == 0) {
+    private void throttle() {
+        if (_maxBytesPerSecond <= 0) {
             return;
         }
-
-        long diffBytes = _counter - _lastCounter;
-        long millis = System.currentTimeMillis();
-        long diffMillis = millis - _lastMillis;
-        long waitMillis = (1000 * diffBytes - diffMillis * _maxBytesPerSecond) / _maxBytesPerSecond;
-
-        if (waitMillis > 0) {
+        while (getBytesPerSec() > _maxBytesPerSecond) {
             synchronized (_monitor) {
                 try {
-                    _monitor.wait(waitMillis);
-                } catch (InterruptedException ie) {
+                    _monitor.wait(SLEEP_DURATION_MS);
+                    _totalSleepTime += SLEEP_DURATION_MS;
+                } catch (InterruptedException e) {
+                    logger.warn("Caught InterruptedException while throttling", e);
+                    break;
                 }
             }
         }
+    }
 
-        _lastCounter = _counter;
-        _lastMillis = System.currentTimeMillis();
+    public long getTotalBytesRead() {
+        return _bytesRead;
+    }
+
+    /**
+     * Return the number of bytes read per second
+     */
+    public long getBytesPerSec() {
+        long elapsed = (System.nanoTime() - _startTime) / 1000000000;
+        if (elapsed == 0) {
+            return _bytesRead;
+        } else {
+            return _bytesRead / elapsed;
+        }
+    }
+
+    public long getTotalSleepTime() {
+        return _totalSleepTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ThrottledInputStream{bytesRead=");
+        sb.append(getTotalBytesRead());
+        sb.append(", maxBytesPerSec=");
+        sb.append(_maxBytesPerSecond);
+        sb.append(", bytesPerSec=");
+        sb.append(getBytesPerSec());
+        sb.append(", totalSleepTimeInSeconds=");
+        if (getTotalSleepTime() == 0) {
+            sb.append('0');
+        } else {
+            sb.append(getTotalSleepTime() / 1000);
+        }
+        return sb.toString();
     }
 }
