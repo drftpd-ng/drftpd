05eb7c0e365cd414f664d5ba9c6f0358b4e11e51
zc0nf <10996782+zc0nf@users.noreply.github.com>
2021-12-01 21:51:59 +0100

    HEAP: Adding Xms which is equal to Xmx
    - By adding a minimum size via Xms, the master as well as the slave must have the specified size available otherwise the application will crash.
    - The Xms/Xmx values ​​will have to be adjusted to your environment, an empirical analysis will make it possible to best define this necessary size

--- src/core/slave/src/main/resources/slave/slave.sh
+++ src/core/slave/src/main/resources/slave/slave.sh
@@ -50,7 +50,7 @@
 
 CLASSPATH="lib/*:build/*"
 # Add JVM Options here however you see fit and please check if the max memory Xmx is good enough for your slave.
-JVM_OPTS="-Xmx1G -XX:+UseZGC"
+JVM_OPTS="-Xms1G -Xmx1G -XX:+UseZGC"
 OPTIONS="-Dlog4j.configurationFile=config/log4j2-slave.xml"
 PROGRAM="org.drftpd.slave.Slave"
 
